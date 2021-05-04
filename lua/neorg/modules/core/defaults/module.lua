ea74e4fe7d3a52617df1f10b69a188b78f42f3d4
Vhyrro <vhyrro@gmail.com>
2021-05-04 15:45:09 +0200

    fix: actually add the default metamodule file, my bad

---
+++ lua/neorg/modules/core/defaults/module.lua
@@ -0,0 +1,8 @@
+--[[
+-- DEFAULTS METAMODULE FOR NEORG
+-- Houses all the default modules that an everday user may want for a nice user experience.
+--]]
+
+require('neorg.modules.base')
+
+return neorg.modules.create_meta("core.defaults", "core.neorgcmd")
