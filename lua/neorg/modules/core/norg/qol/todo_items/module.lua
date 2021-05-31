16790eb3c4e59e4882b6cbeebf957b2bf90b9e34
Vhyrro <vhyrro@gmail.com>
2021-05-31 19:07:42 +0200

    docs: add comments to code and small formatting fixes

--- lua/neorg/modules/core/norg/qol/todo_items/module.lua
+++ lua/neorg/modules/core/norg/qol/todo_items/module.lua
@@ -8,7 +8,7 @@
     - todo.task_cycle
 
     TODO: get rid of the pattern matching stuff within the on_event() local functions once the scanner module is ready
-    TODO: use sym.states withing the on_event() local funtions once global utility functions become a thing
+    TODO: use sym.states within the on_event() local funtions once global utility functions become a thing
     TODO: make events name processing like a domain name such as making:
 
 		["core.norg.qol.todo_items.todo.task_done"] = true,
@@ -18,11 +18,11 @@
 
     The same as:
 
-        ["core.norg.qol.todo_items.todo] = {
-            [task_done"] = true
-            [task_undone"] = true
-            [task_pending"] = true
-            [task_cycle"] = true
+        ["core.norg.qol.todo_items.todo"] = {
+            ["task_done"] = true
+            ["task_undone"] = true
+            ["task_pending"] = true
+            ["task_cycle"] = true
         }
 --]]
 
@@ -59,39 +59,58 @@ module.on_event = function(event)
 
 	if event.split_type[1] == "core.keybinds" then
 
+		-- @Summary Sets the current todo item's state
+		-- @Param  state (string) - a string of characters to replace the current todo state
 		local set_todo_item_state = function(state)
+			-- Grab the current line
 			local current_line = vim.api.nvim_get_current_line()
+
+			-- Before you die of a heart attack, the below regex is supposed to pattern match a todo item,
+			-- for example one that looks like this: - [x] Example!
 			local str, _ = current_line:gsub("^(%s*%-%s+%" .. sym.left_bracket .. "%s*)[x%*%s](%s*%"..sym.right_bracket.."%s+)", "%1" .. state .. "%2", 1)
+
+			-- If the current line differs from what we already have then change it!
 			if current_line ~= str then vim.api.nvim_set_current_line(str) end
 		end
 
+		-- @Summary Gets the current todo item's state
+		-- @Description Pattern matches the current line to query the current todo item.
 		local get_todo_item_state = function()
-			return vim.api.nvim_get_current_line():match("^%s*%-%s+%" .. sym.left_bracket .. "%s*([x%*%s])%s*%"..sym.right_bracket.."%s+")
+			return vim.api.nvim_get_current_line():match("^%s*%-%s+%" .. sym.left_bracket .. "%s*([x%*%s])%s*%" .. sym.right_bracket .. "%s+")
 		end
 
+		-- @Summary Cycles todo items
+		-- @Description Queries the todo item on the current line and cycles it between a predefined list of states
 		local cycle_todo_item = function()
+			-- Define that states and query the current state
 			local states = { " ", "*", "x" }
 			local next_state = get_todo_item_state()
 
+			-- If the current state cannot be found (ie. the current line is not a valid todo item) then don't do anything
 			if not next_state then return end
 
+			-- Loop through all values and find the next element
 			for i, state in ipairs(states) do
+				-- If we have found the current element
 				if next_state == state then
+					-- Then either wrap around to the beginning of the table if we're reading too far into it
 					if i == #states then
 						i = 1
-					else
+					else -- Or advance the i variable by one
 						i = i + 1
 					end
 
+					-- Define the next todo item state and exit the loop, everything was successful
 					next_state = states[i]
-
 					break
 				end
 			end
 
+			-- Actually set the item state
             set_todo_item_state(next_state)
 		end
 
+		-- Depending on the event received perform different todo actions
 		if event.split_type[2] == "core.norg.qol.todo_items.todo.task_done" then
 			set_todo_item_state(sym.states.done)
 		elseif event.split_type[2] == "core.norg.qol.todo_items.todo.task_undone" then
