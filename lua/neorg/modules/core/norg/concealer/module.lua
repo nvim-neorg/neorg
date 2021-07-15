311e055a6078d024daa21874d0b871ceb4c26026
Vhyrro <vhyrro@gmail.com>
2021-07-15 21:40:31 +0200

    New snazzy features:     - Overhaul `core.norg.concealer` completely. New config options, new       icons, but most importantly - new engine     - The new engine uses extmarks rather than regular conceals (props       to akinsho for letting me know that you can do that) and gives so much more       flexibility. One of the main things is being able to change the       colour of individual icons and allow different icons to span       across more than one character     - Add an icon for markers
    Former-commit-id: fdd9014cbc7de53e8d7c55843e06ffea910a57c3

--- lua/neorg/modules/core/norg/concealer/module.lua
+++ lua/neorg/modules/core/norg/concealer/module.lua
@@ -2,7 +2,7 @@
 	CONCEALER MODULE FOR NEORG.
 	This module is supposed to enhance the neorg editing experience
 	by abstracting away certain bits of text and concealing it into one easy-to-recognize
-	icon. Icons can be easily changed and most elements can be disabled.
+	icon. Icons can be easily changed and every element can be disabled.
 
 USAGE:
 	This module does not come bundled by default with the core.defaults metamodule.
@@ -59,10 +59,14 @@ USAGE:
 			enabled = false,
 			icon = "‑"
 		},
-	},
+	}
 
-	-- The value to set `concealcursor` to (see `:h concealcursor`)
-	conceal_cursor = ""
+	You can also add your own custom conceals with their own custom icons, however this is a tad more complex.
+
+	Note that those are probably the configuration options that you are *going* to use.
+	There are a lot more configuration options per element than that, however.
+
+	TODO: Complete docs
 --]]
 
 require('neorg.modules.base')
@@ -70,13 +74,14 @@ require('neorg.modules.base')
 local module = neorg.modules.create("core.norg.concealer")
 
 module.setup = function()
-	return { success = true, requires = { "core.autocommands" } }
+	return { success = true, requires = { "core.autocommands", "core.highlights" } }
 end
 
-module.load = function()
-	module.required["core.autocommands"].enable_autocommand("BufEnter")
-	module.public.trigger_conceal()
-end
+module.private = {
+	namespace = vim.api.nvim_create_namespace("neorg_conceals"),
+	extmarks = {},
+	icons = {}
+}
 
 module.config.public = {
 
@@ -86,132 +91,214 @@ module.config.public = {
 
 			done = {
 				enabled = true,
-				icon = ""
+				icon = "",
+				force_length = 1,
+				pattern = "^(%s*%-%s+%[%s*)x%s*%]%s+",
+				whitespace_index = 1,
+				highlight = "NeorgTodoItemDoneMark",
+				padding_before = 0,
 			},
+
 			pending = {
 				enabled = true,
-				icon = ""
+				icon = "",
+				force_length = 1,
+				pattern = "^(%s*%-%s+%[%s*)%*%s*%]%s+",
+				whitespace_index = 1,
+				highlight = "NeorgTodoItemPendingMark",
+				padding_before = 0,
 			},
+
 			undone = {
 				enabled = true,
-				icon = "×"
+				icon = "×",
+				force_length = 1,
+				pattern = "^(%s*%-%s+%[)%s+]%s+",
+				whitespace_index = 1,
+				highlight = "TSComment",
+				padding_before = 0,
 			}
 		},
+
 		quote = {
 			enabled = true,
-			icon = "∣"
+			icon = "∣",
+			force_length = 1,
+			pattern = "^(%s*)>%s+",
+			whitespace_index = 1,
+			highlight = "NeorgQuote",
+			padding_before = 0,
 		},
+
 		heading = {
 			enabled = true,
 
 			level_1 = {
 				enabled = true,
-				icon = "⦿"
+				icon = "◉",
+				force_length = 1,
+				pattern = "^(%s*)%*%s+",
+				whitespace_index = 1,
+				highlight = "NeorgHeading1",
+				padding_before = 0,
 			},
 
 			level_2 = {
 				enabled = true,
-				icon = "⦾"
+				icon = "○",
+				force_length = 1,
+				pattern = "^(%s*)%*%*%s+",
+				whitespace_index = 1,
+				highlight = "NeorgHeading2",
+				padding_before = 1,
 			},
 
 			level_3 = {
 				enabled = true,
-				icon = "•"
+				icon = "✿",
+				force_length = 1,
+				pattern = "^(%s*)%*%*%*%s+",
+				whitespace_index = 1,
+				highlight = "NeorgHeading3",
+				padding_before = 2,
 			},
 
 			level_4 = {
 				enabled = true,
-				icon = "◦"
+				icon = "•",
+				force_length = 1,
+				pattern = "^(%s*)%*%*%*%*%s+",
+				whitespace_index = 1,
+				highlight = "NeorgHeading4",
+				padding_before = 3,
 			},
 		},
-		list = {
-			enabled = false,
-			icon = "‑"
+
+		marker = {
+			enabled = true,
+			icon = "",
+			force_length = 1,
+			pattern = "^(%s*)%|%s+",
+			whitespace_index = 1,
+			highlight = "NeorgMarker",
+			padding_before = 0,
 		},
 	},
 
-	conceal_cursor = ""
-
 }
+
+module.load = function()
+	local get_enabled_icons
+
+	get_enabled_icons = function(tbl)
+		local result = {}
+
+		if tbl.enabled ~= nil and tbl.enabled == false then
+			return result
+		end
+
+		for name, icons in pairs(tbl) do
+			if type(icons) == "table" and icons.enabled then
+				if icons.icon then
+					result[name] = icons
+				else
+					result = vim.tbl_deep_extend("force", result, get_enabled_icons(icons))
+				end
+			end
+		end
+
+		return result
+	end
+
+	module.private.icons = vim.tbl_values(get_enabled_icons(module.config.public.icons))
+
+	-- log.warn(module.private.icons)
+
+	module.required["core.autocommands"].enable_autocommand("BufEnter")
+	module.required["core.autocommands"].enable_autocommand("TextChanged")
+	module.required["core.autocommands"].enable_autocommand("TextChangedI")
+
+	module.public.trigger_conceal()
+end
 
 module.public = {
 
 	-- @Summary Activates concealing for the current window
 	-- @Description Parses the user configuration and enables concealing for the current window.
 	trigger_conceal = function()
+		module.public.clear_conceal()
 
-		vim.schedule(function()
+		local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-			-- @Summary Creates a new conceal
-			-- @Description Constructs a new match with regex via :syntax match
-			-- @Param  name (string) - the name of the match
-			-- @Param  regex (string) - the regex to match against
-			-- @Param  tbl (table) - the table that contains an `enabled` and `icon` variable
-			local create_syntax_match = function(name, regex, tbl)
-				if tbl.enabled then
-					vim.cmd("syntax match " .. name .. " " .. regex .. " conceal oneline cchar=" .. tbl.icon)
+		for i, line in ipairs(lines) do
+			module.public.set_extmark(i, line)
+		end
+	end,
+
+	update_current_line = function()
+		local line_number = vim.api.nvim_win_get_cursor(0)[1]
+
+		vim.api.nvim_buf_clear_namespace(0, module.private.namespace, line_number - 1, line_number)
+
+		module.public.set_extmark(line_number, vim.api.nvim_get_current_line())
+	end,
+
+	set_extmark = function(line_number, line)
+		for _, icon_info in ipairs(module.private.icons) do
+			if icon_info.pattern then
+				local match = { line:match(icon_info.pattern) }
+
+				if not vim.tbl_isempty(match) then
+					local whitespace_amount = match[icon_info.whitespace_index]:len()
+					local full_icon = (" "):rep(icon_info.padding_before) .. icon_info.icon
+					local icon_length = (icon_info.force_length and icon_info.force_length or icon_info.icon:len())
+
+					module.public._set_extmark(full_icon, icon_info.highlight, line_number - 1, whitespace_amount, whitespace_amount + icon_length)
 				end
 			end
+		end
+	end,
 
-			-- Enable concealing for the current buffer
-			vim.wo.conceallevel = 2
+	_set_extmark = function(text, highlight, line_number, start_column, end_column)
+ 		local ok, result = pcall(vim.api.nvim_buf_set_extmark, 0, module.private.namespace, line_number, start_column,
+ 		{
+    		end_col = end_column,
+    		hl_group = highlight,
+    		virt_text = { { text, highlight } },
+    		virt_text_pos = "overlay",
+    		hl_mode = "combine",
+  		})
 
-			-- Set the concealcursor as requested
-			vim.wo.concealcursor = module.config.public.conceal_cursor
-
-			-- Define syntax for all the concealable characters
-
-			create_syntax_match("NeorgUnorderedList", [[ /\%\(^\s*\)\@<=\-\%\(\s\+\)\@=/ ]], module.config.public.icons.list)
-
-			create_syntax_match("NeorgQuote", [[ /\%\(^\s*\)\@<=>\%\(\s\+\)\@=/ ]], module.config.public.icons.quote)
-
-			if module.config.public.icons.heading.enabled then
-				create_syntax_match("NeorgHeading1", [[ /\%\(^\s*\)\@<=\*\%\(\s\+\)\@=/ ]], module.config.public.icons.heading.level_1)
-				create_syntax_match("NeorgHeading2", [[ /\%\(^\s*\)\@<=\*\*\%\(\s\+\)\@=/ ]], module.config.public.icons.heading.level_2)
-				create_syntax_match("NeorgHeading3", [[ /\%\(^\s*\)\@<=\*\*\*\%\(\s\+\)\@=/ ]], module.config.public.icons.heading.level_3)
-				create_syntax_match("NeorgHeading4", [[ /\%\(^\s*\)\@<=\*\*\*\*\%\(\s\+\)\@=/ ]], module.config.public.icons.heading.level_4)
-			end
-
-			if module.config.public.icons.todo.enabled then
-				create_syntax_match("NeorgTaskDone", [[ /\%\(^\s*\-\s\+\)\@<=\[\s*x\s*\]\%\(\s\+\)\@=/ ]], module.config.public.icons.todo.done)
-				create_syntax_match("NeorgTaskUndone", [[ /\%\(^\s*\-\s\+\)\@<=\[\s\+]\%\(\s\+\)\@=/ ]], module.config.public.icons.todo.undone)
-				create_syntax_match("NeorgTaskPending", [[ /\%\(^\s*\-\s\+\)\@<=\[\s*\*\s*\]\%\(\s\+\)\@=/ ]], module.config.public.icons.todo.pending)
-			end
-
-		end)
-
+  		if not ok then
+    		log.error("Unable to create custom conceal for highlight:", highlight, "-", result)
+  		else
+			-- module.private.extmarks[line_number] = module.private.extmarks[line_number] or {}
+  		end
 	end,
 
 	-- @Summary Clears all the conceals that neorg has defined
 	-- @Description Uses the `:syntax clear` command to remove all active conceals
 	clear_conceal = function()
-		vim.schedule(function()
-			vim.cmd [[
-				silent! syntax clear NeorgUnorderedList
-				silent! syntax clear NeorgQuote
-				silent! syntax clear NeorgHeading1
-				silent! syntax clear NeorgHeading2
-				silent! syntax clear NeorgHeading3
-				silent! syntax clear NeorgHeading4
-				silent! syntax clear NeorgTaskDone
-				silent! syntax clear NeorgTaskUndone
-				silent! syntax clear NeorgTaskPending
-			]]
-		end)
+		vim.api.nvim_buf_clear_namespace(0, module.private.namespace, 0, -1)
 	end
 
 }
 
 module.on_event = function(event)
-	if event.type == "core.autocommands.events.bufenter" then
+	if event.type == "core.autocommands.events.bufenter" and event.content.norg then
 		module.public.trigger_conceal()
+	elseif event.type == "core.autocommands.events.textchanged" or event.type == "core.autocommands.events.textchangedi" then
+		-- Just temporary
+		module.public.update_current_line()
 	end
 end
 
 module.events.subscribed = {
 
 	["core.autocommands"] = {
-		bufenter = true
+		bufenter = true,
+		textchangedi = true,
+		textchanged = true
 	}
 
 }
