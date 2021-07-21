8934034776d7d499b67a93cd01d82cb137414e7e
Vhyrro <vhyrro@gmail.com>
2021-07-21 10:22:20 +0200

    Indentation changes:     - Added realtime indenting:       - Neorg can now indent your lines as you type instead of       	statically calculating the indent amount and forcing you to use       	`=` to indent       - Realtime indentation is configurable and doesn't slow down your       	text editing     - Moved all current indents to a new section - previous-line       indents. Previous line indents are those that look at look at the       previous lines in your document to calculate how much to indent       the current line     - Added new indent section - current line indents. Contrary to       previous line indents, current line indents simply look at the       current line to calculate the indent amount. This is useful for       e.g. headings that will always be indented the same way.

--- lua/neorg/modules/core/norg/esupports/module.lua
+++ lua/neorg/modules/core/norg/esupports/module.lua
@@ -16,43 +16,6 @@ require('neorg.modules.base')
 local module = neorg.modules.create("core.norg.esupports")
 
 function _neorg_indent_expr()
-	-- @Summary Creates a new indent
-	-- @Description Sets a new set of rules that when fulfilled will indent the text properly
-	-- @Param  match (string) - a regex that should match the line above the newly placed line
-	-- @Param  indent (function(matches) -> number) - a function that should return the level of indentation in spaces for that line
-	-- @Param  current (boolean) - if true checks the current line rather than the previous non-blank line
-	local create_indent = function(match, indent, current)
-
-		local line_number = current and vim.api.nvim_win_get_cursor(0)[1] or vim.fn.prevnonblank(vim.api.nvim_win_get_cursor(0)[1] - 1)
-
-		-- If the line number above us is 0 then don't indent anything
-		if line_number == 0 then
-			return 0
-		end
-
-		-- nvim_buf_get_lines() doesn't work here for some reason :(
-		local line = vim.fn.getline(line_number)
-
-		-- Pack all the matches into this lua table
-		local matches = { line:match("^(" .. match .. ")$") }
-
-		-- If we have indenting enabled and if the match is successful
-		if module.config.public.indent and matches[1] and matches[1]:len() > 0 then
-			-- Invoke the callback for indenting
-			local indent_amount = indent(vim.list_slice(matches, 2))
-
-			-- If the return value of the callback is -1, make neovim automatically indent the next line
-			-- Else, use the returned indent amount to calculate a new value, one that will work with any
-			-- size of tabs
-			indent_amount = indent_amount == false and vim.fn.indent(line) or indent_amount + (vim.fn.strdisplaywidth(line) - line:len())
-
-			-- Return success
-			return indent_amount, true
-		end
-
-		-- If we haven't found a match, return nothing
-		return nil, false
-	end
 
 	local indent_amount, success
 
@@ -61,7 +24,7 @@ function _neorg_indent_expr()
 		for _, data in pairs(module.config.public.indent_config.current) do
 			if type(data) == "table" and data.enabled then
 				-- Check whether the line matches any of our criteria
-				indent_amount, success = create_indent(data.regex, data.indent, true)
+				indent_amount, success = module.public.create_indent(data.regex, data.indent, true)
 				-- If it does, then return that indent!
 				if success then return indent_amount end
 			end
@@ -73,7 +36,7 @@ function _neorg_indent_expr()
 		for _, data in pairs(module.config.public.indent_config.previous) do
 			if type(data) == "table" and data.enabled then
 				-- Check whether the line matches any of our criteria
-				indent_amount, success = create_indent(data.regex, data.indent, false)
+				indent_amount, success = module.public.create_indent(data.regex, data.indent, false)
 				-- If it does, then return that indent!
 				if success then return indent_amount end
 			end
@@ -95,13 +58,37 @@ module.config.public = {
 		current = {
 			enabled = true,
 
-			heading = {
+			heading1 = {
 				enabled = true,
 				regex = "(%s*%*%s+)(.*)",
 				indent = function()
-					return -1
+					return 0
 				end
-			}
+			},
+
+			heading2 = {
+				enabled = true,
+				regex = "(%s*%*%*%s+)(.*)",
+				indent = function()
+					return 1
+				end
+			},
+
+			heading3 = {
+				enabled = true,
+				regex = "(%s*%*%*%*%s+)(.*)",
+				indent = function()
+					return 2
+				end
+			},
+
+			heading4 = {
+				enabled = true,
+				regex = "(%s*%*%*%*%*%s+)(.*)",
+				indent = function()
+					return 3
+				end
+			},
 		},
 
 		previous = {
@@ -122,7 +109,7 @@ module.config.public = {
 					if matches[2]:len() > 0 then
 						return matches[1]:len()
 					else
-						return false
+						return -1
 					end
 				end
 			},
@@ -139,6 +126,37 @@ module.config.public = {
 		realtime = {
 			enabled = true,
 
+			heading1 = {
+				enabled = true,
+				regex = "%s*%*%s+(.*)",
+				indent = function()
+					return 0
+				end
+			},
+
+			heading2 = {
+				enabled = true,
+				regex = "%s*%*%*%s+(.*)",
+				indent = function()
+					return 1
+				end
+			},
+
+			heading3 = {
+				enabled = true,
+				regex = "%s*%*%*%*%s+(.*)",
+				indent = function()
+					return 2
+				end
+			},
+
+			heading4 = {
+				enabled = true,
+				regex = "%s*%*%*%*%*%s+(.*)",
+				indent = function()
+					return 3
+				end
+			},
 		}
 	},
 
@@ -150,9 +168,51 @@ module.config.public = {
 
 module.load = function()
 	module.required["core.autocommands"].enable_autocommand("BufEnter")
+	module.required["core.autocommands"].enable_autocommand("TextChangedI")
+	module.required["core.autocommands"].enable_autocommand("BufWrite")
 end
 
 module.public = {
+
+	-- @Summary Creates a new indent
+	-- @Description Sets a new set of rules that when fulfilled will indent the text properly
+	-- @Param  match (string) - a regex that should match the line above the newly placed line
+	-- @Param  indent (function(matches) -> number) - a function that should return the level of indentation in spaces for that line
+	-- @Param  current (boolean) - if true checks the current line rather than the previous non-blank line
+	create_indent = function(match, indent, current)
+
+		local line_number = current and vim.api.nvim_win_get_cursor(0)[1] or vim.fn.prevnonblank(vim.api.nvim_win_get_cursor(0)[1] - 1)
+
+		-- If the line number above us is 0 then don't indent anything
+		if line_number == 0 then
+			return 0
+		end
+
+		-- nvim_buf_get_lines() doesn't work here for some reason :(
+		local line = vim.fn.getline(line_number)
+
+		-- Pack all the matches into this lua table
+		local matches = { line:match("^(" .. match .. ")$") }
+
+		-- If the match is successful
+		if matches[1] and matches[1]:len() > 0 then
+			-- Invoke the callback for indenting
+			local indent_amount = indent(vim.list_slice(matches, 2))
+
+			if indent_amount == -1 then
+				indent_amount = vim.fn.indent(line)
+			elseif not current then
+				indent_amount = indent_amount + (vim.api.nvim_strwidth(line) - line:len())
+			end
+
+			-- Return success
+			return indent_amount, true
+		end
+
+		-- If we haven't found a match, return nothing
+		return nil, false
+	end,
+
 	-- @Summary Creates metadata for the current file
 	-- @Description Pastes a @document.meta block at the top of the current document
 	construct_metadata = function()
@@ -171,13 +231,50 @@ module.public = {
 		}, "l", false, true)
 
 		vim.opt_local.modified = false
+	end,
+
+	-- @Summary Indents the current line
+	-- @Description Performs real-time indentation of the current line
+	indent_line = function()
+		-- Loop through all the data present in the indent configuration
+		for _, data in pairs(module.config.public.indent_config.realtime) do
+			-- If the data we're dealing with is correct and it's enabled then
+			if type(data) == "table" and data.enabled then
+				-- Get the indent amount for the current line
+				local indent_amount, success = module.public.create_indent(data.regex, data.indent, true)
+
+				-- If we've managed to successfully indent the current line
+				if success then
+					-- Cache the current line (before any changes)
+					local cursor_pos = vim.api.nvim_win_get_cursor(0)
+
+					-- Set the indentation level for the current line
+					local line = vim.api.nvim_get_current_line()
+					local sub = line:gsub("^%s*", (" "):rep(indent_amount))
+
+					-- If the line has undergone any changes
+					if sub ~= vim.api.nvim_get_current_line() then
+						-- Set the line to the newly indented line
+						vim.api.nvim_set_current_line(sub)
+
+						-- Calculate the difference in chars from before the indentation to set the cursor
+						-- accordingly (otherwise it would get offset in weird ways)
+						vim.api.nvim_win_set_cursor(0, { cursor_pos[1], cursor_pos[2] + (vim.api.nvim_strwidth(vim.api.nvim_get_current_line()) - vim.api.nvim_strwidth(line)) })
+					end
+
+					break
+				end
+			end
+		end
 	end
 }
 
 module.on_event = function(event)
 	if event.type == "core.autocommands.events.bufenter" then
 		if event.content.norg then
-			vim.opt_local.indentexpr = "v:lua._neorg_indent_expr()"
+			if module.config.public.indent then
+				vim.opt_local.indentexpr = "v:lua._neorg_indent_expr()"
+			end
 
 			-- If folds are enabled then handle them
 			if module.config.public.folds.enabled then
@@ -195,11 +292,22 @@ module.on_event = function(event)
 			end
 		end
 	end
+
+	-- If we have changed some text then attempt to auto-indent the current line
+	if event.type == "core.autocommands.events.textchangedi" and module.config.public.indent_config.realtime.enabled then
+		module.public.indent_line()
+	end
+
+	if event.type == "core.autocommands.events.bufwrite" then
+		-- TODO
+	end
 end
 
 module.events.subscribed = {
 	["core.autocommands"] = {
 		bufenter = true,
+		textchangedi = true,
+		bufwrite = false,
 	}
 }
 
