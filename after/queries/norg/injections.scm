ea4109c5d416838dc4d77f11af89461e6d528dfa
Vhyrro <vhyrro@gmail.com>
2021-07-21 18:08:20 +0200

    fix: disallow the Neorg parser from recursively highlighting itself in `@code` blocks
    Former-commit-id: 7689e28196e111a04ed0fc33b4c406d015f0f29e

--- after/queries/norg/injections.scm
+++ after/queries/norg/injections.scm
@@ -1,4 +1,4 @@
-((tag_name) @_tagname (tag_parameters) @language (tag_content) @content (#eq? @_tagname "code"))
+((tag_name) @_tagname (tag_parameters) @language (tag_content) @content (#eq? @_tagname "code") (#not-eq? @language "norg"))
 
 ; Custom injections for language shorthands
 ((tag_name) @_tagname (tag_parameters) @_language (tag_content) @content (#eq? @_language "js") (#set! "language" "javascript"))
