275f1d6352e1bf8ec985c94f83ba7c8cd940d640
Daniel Mathiot <d.danymat@gmail.com>
2021-08-08 20:47:17 +0200

    Allow the user to add a task
    I use core.ui to generate the prompt asking the user to add the task to
    the inbox list.
    Still there are some issues regarding core.ui:
        - after <CR>, the buffer keeps asking for an other user input.
        - If if press <C-c>, the buffer seems to close but the callback
          doesn't start
    
    
    Former-commit-id: d34bae30f01390fe0c208660b1bd26977f43f523

--- lua/neorg/modules/core/gtd/base/module.lua
+++ lua/neorg/modules/core/gtd/base/module.lua
@@ -2,10 +2,12 @@ require("neorg.modules.base")
 require("neorg.events")
 
 local module = neorg.modules.create("core.gtd.base")
-local log = require("neorg.external.log")
 
 module.setup = function ()
-    return { success = true, requires = { 'core.norg.dirman', 'core.keybinds' } }
+    return {
+        success = true,
+        requires = { 'core.norg.dirman', 'core.keybinds', 'core.ui' }
+    }
 end
 
 module.config.public = {
@@ -13,34 +15,67 @@ module.config.public = {
 }
 
 module.private = {
-    workspace = nil,
+    workspace_full_path = nil,
     default_lists = {
         inbox = "INBOX.norg",
         projects = "PROJECTS.norg",
         someday = "SOMEDAY.norg"
-    }
+    },
+
+-- @Summary Append text to list
+-- @Description Append the text to the specified list (defined in private.default_lists)
+-- @Param  list (string) the list to use
+-- @Param  text (string) the text to append
+    add_to_list = function (list, text)
+        local fn = io.open(module.private.workspace_full_path .. "/" .. list, "a")
+        fn:write(text)
+        fn:flush()
+        fn:close()
+    end
 }
 
 module.load = function ()
-    --module.public.add_task_to_inbox()
-    module.private.workspace = module.config.public.workspace or "default"
-    log.info('Loaded GTD')
+    -- Get workspace for gtd files and save full path in private
+    local workspace = module.config.public.workspace or "default"
+    module.private.workspace_full_path = module.required["core.norg.dirman"].get_workspace(workspace)
+
+    -- Register keybinds
     module.required["core.keybinds"].register_keybind(module.name, "add_to_inbox")
 end
 
 module.on_event = function (event)
     if event.split_type[2] == "core.gtd.base.add_to_inbox" then
-       module.public.add_task_to_inbox()
+        module.public.add_task_to_inbox()
     end
 end
 
 module.public = {
     version = "0.1",
 
+-- @Summary Add user task to inbox
+-- @Description Show prompt asking for user input and append the task to the inbox
     add_task_to_inbox = function ()
-        workspace_path = module.required["core.norg.dirman"].get_workspace(module.private.workspace)
-        log.info("Inbox path:", workspace_path .. "/" .. module.private.default_lists.inbox)
-        log.info("Added to INBOX")
+        -- Define a callback (for prompt) to add the task to the inbox list
+        local cb = function (text)
+            module.private.add_to_list(module.private.default_lists.inbox, "- [ ] " .. text .. "\n")
+        end
+
+        -- Show prompt asking for input
+        module.required["core.ui"].create_prompt(
+            "INBOX_WINDOW",
+            "Add to inbox.norg: ",
+            cb,
+            {
+                center_x = true,
+                center_y = true,
+            },
+            {
+                width = 60,
+                height = 1,
+                row = 1,
+                col = 1
+            })
+
     end
 }
 
