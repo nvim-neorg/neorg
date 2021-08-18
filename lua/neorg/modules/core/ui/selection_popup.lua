aad6c0013a74db4b655627c94a0c03d76d2663c7
Vhyrro <vhyrro@gmail.com>
2021-08-18 16:39:09 +0200

    feat: add support for capturing inputs, it's a bit broken
    Former-commit-id: 28ff00ede0b1915f275765ccc7db340abbe1c281

--- lua/neorg/modules/core/ui/selection_popup.lua
+++ lua/neorg/modules/core/ui/selection_popup.lua
@@ -5,94 +5,111 @@
 return function(module)
     return {
 
-        create_selection = function(name, config)
+        create_selection = function(name, config, callback)
             vim.validate({
                 name = { name, "string" },
                 config = { config, "table" },
+                callback = { callback, "function" },
             })
 
-            local buf = module.public.create_split("selection/" .. name, config.buffer_options, function(buf)
-                -- TODO: refactor out the vim.tbl_keys
+            local buf = module.public.create_split("selection/" .. name, config.buffer_options)
+
+            local function display_values(flags)
+                vim.api.nvim_buf_clear_namespace(buf, module.private.namespace, 0, -1)
+
                 vim.api.nvim_buf_set_lines(
                     buf,
                     0,
-                    #vim.tbl_keys(config.flags) + 1,
+                    #vim.tbl_keys(flags) + 1,
                     false,
-                    vim.split(("\n"):rep(#vim.tbl_keys(config.flags) + 1), "\n", true)
+                    vim.split(("\n"):rep(#vim.tbl_keys(flags) + 1), "\n", true)
                 )
-            end)
 
-            if buf == -1 then
-                log.error("Unable to create selection, buffer with name", name, "not found")
-                return
-            end
+                local text_for_current = (function()
+                    local result = {
+                        { { name .. ":", "TSAnnotation" } },
+                        { { " ", "Normal" } },
+                    }
 
-            local text_for_current = (function()
-                local result = {
-                    { { name .. ":", "TSAnnotation" } },
-                    { { " ", "Normal" } },
-                }
+                    for keybind, value in pairs(flags) do
+                        local keybind_element = {}
 
-                for keybind, value in pairs(config.flags) do
-                    local keybind_element = {}
+                        table.insert(keybind_element, { keybind, "TSType" })
+                        table.insert(keybind_element, { " -> ", "Normal" })
 
-                    table.insert(keybind_element, { keybind, "TSType" })
-                    table.insert(keybind_element, { " -> ", "Normal" })
+                        if type(value) == "table" then
+                            table.insert(keybind_element, { value.name or "no description", "TSMath" })
+                        elseif type(value) == "string" then
+                            table.insert(keybind_element, { value, "TSMath" })
+                        else
+                            table.insert(keybind_element, { tostring(value), "TSMath" })
+                        end
 
-                    if type(value) == "table" then
-                        table.insert(keybind_element, { value.name or "no description", "TSMath" })
-                    elseif type(value) == "string" then
-                        table.insert(keybind_element, { value, "TSMath" })
-                    else
-                        table.insert(keybind_element, { tostring(value), "TSMath" })
+                        table.insert(result, keybind_element)
                     end
 
-                    table.insert(result, keybind_element)
+                    return result
+                end)()
+
+                for i, virt_text in ipairs(text_for_current) do
+                    vim.api.nvim_buf_set_extmark(buf, module.private.namespace, i - 1, 0, {
+                        virt_text = virt_text,
+                        virt_text_pos = "overlay",
+                    })
                 end
-
-                return result
-            end)()
-
-            for i, virt_text in ipairs(text_for_current) do
-                vim.api.nvim_buf_set_extmark(buf, module.private.namespace, i - 1, 0, {
-                    virt_text = virt_text,
-                    virt_text_pos = "overlay",
-                })
             end
+
+            display_values(config.flags)
+
+            local location, result = config.flags, {}
+
+            -- WARNING: Idk why but for some reason this gets executed before the buffer is displayed, what??
+            while location do
+                local input = vim.fn.getcharstr()
+
+                if location[input] then
+                    table.insert(result, input)
+
+                    if type(location[input]) == "string" then
+                        callback(result)
+                        break
+                    else
+                        if not location[input].flags then
+                            log.error(
+                                'Malformed input provided to create_selection: expected a "flags" variable in subtable'
+                            )
+                            break
+                        end
+
+                        location = location[input].flags
+                        display_values(location)
+                    end
+                end
+            end
+
+            vim.api.nvim_buf_delete(buf, { force = true })
         end,
 
         -- Creates a split and returns the buffer ID contained within the split
-        create_split = function(name, config, callback)
-            if vim.fn.bufexists("neorg://" .. name) == 1 then
-                return vim.fn.bufnr("neorg://" .. name)
-            end
-
+        create_split = function(name, config)
             vim.validate({
                 name = { name, "string" },
                 config = { config, "table", true },
-                callback = { callback, "function", true },
             })
 
-            vim.cmd("split neorg://" .. name)
+            vim.cmd("below new")
 
-            local buf = vim.fn.bufnr("neorg://" .. name)
-
-            if buf == -1 then
-                return buf
-            end
+            local buf = vim.api.nvim_get_current_buf()
 
             local default_options = {
                 modified = false,
-                modifiable = false,
                 buflisted = false,
+                swapfile = false,
+                bufhidden = "hide",
+                buftype = "nofile",
             }
 
-            if callback then
-                callback(buf)
-            end
-
-            vim.api.nvim_win_set_buf(0, buf)
-
+            vim.api.nvim_buf_set_name(buf, "neorg://" .. name)
             module.public.apply_buffer_options(buf, vim.tbl_extend("keep", config or {}, default_options))
 
             return buf
