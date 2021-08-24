99e35736daa09ae10aea7b1c2004e3fbd6d595cc
Vhyrro <vhyrro@gmail.com>
2021-08-24 17:42:48 +0200

    feat: make `A` and `B` work cross-file
    Former-commit-id: 09ff51a0df7b51bdbb0f19f8dce00fb57704ece3

--- lua/neorg/modules/core/integrations/treesitter/module.lua
+++ lua/neorg/modules/core/integrations/treesitter/module.lua
@@ -531,6 +531,14 @@ module.public = {
 
     -- Gets the range of a given node
     get_node_range = function(node)
+        if not node then
+        	return {
+        		row_start = 0,
+        		column_start = 0,
+        		row_end = 0,
+        		column_end = 0,
+        	}
+        end
         local rs, cs, re, ce = node:range()
         return {
             row_start = rs,
