223acebea1c3578ae58c7dc578030b2670c4ef3d
Jozef Grajciar <jozef.grajciar@gmail.com>
2021-08-29 15:38:23 +0200

    feat(helpers): helper to get package path

--- lua/neorg/external/helpers.lua
+++ lua/neorg/external/helpers.lua
@@ -405,6 +405,14 @@ neorg.lib = {
     lazy_string_concat = function(...)
         return table.concat({ ... })
     end,
+
+    get_package_path = function()
+        -- Path to this source file, removing the leading '@'
+        local source = string.sub(debug.getinfo(1, "S").source, 2)
+
+        -- Path to the package root
+        return vim.fn.fnamemodify(source, ":p:h:h:h:h")
+    end,
 }
 
 return neorg.utils
