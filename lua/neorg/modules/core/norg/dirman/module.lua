9b4d93a2b7b2148da4fe5552461cecad83347a7f
Daniel Mathiot <d.danymat@gmail.com>
2021-08-31 16:27:55 +0200

    Add files functions in dirman

--- lua/neorg/modules/core/norg/dirman/module.lua
+++ lua/neorg/modules/core/norg/dirman/module.lua
@@ -359,6 +359,36 @@ module.public = {
             end)
         end)
     end,
+    
+    --- Checks for file existence inside a `workspace`, by supplying a relative path in `filepath`
+    --- @param filepath string
+    --- @param workspace_name string
+    file_exists = function (filepath, workspace_name)
+        local workspace = module.public.get_workspace(workspace_name)
+        if workspace == nil then
+            return
+        end
+
+        local f = io.open(workspace .. "/" .. filepath, "r")
+        
+        if f ~= nil then
+           f:close()
+           return true
+        else
+            return false
+        end
+    end,
+
+    --- Get file (with a specific `mode`) for a file specified by `file_path` in `workspace`.
+    --- @param file_path string
+    --- @param workspace_name string
+    --- @param mode string
+    get_fd = function (file_path, workspace_name, mode)
+        if module.public.file_exists(file_path, workspace_name) then
+            local workspace = module.public.get_workspace(workspace_name)
+            return io.open(workspace .. "/" .. file_path, mode)
+        end
+    end
 }
 
 module.on_event = function(event)
