0c30417c501416dd2f4aeb5f1ae32a6827852878
Daniel Mathiot <d.danymat@gmail.com>
2021-09-06 15:26:12 +0200

    Change task tables for processing
    Former-commit-id: 488a48adec2efd28940eb606a37e0f7753f7c6a9

--- lua/neorg/modules/core/gtd/base/displayers.lua
+++ lua/neorg/modules/core/gtd/base/displayers.lua
@@ -1,30 +1,44 @@
-return function (module)
+return function(module)
     return {
-        display_today_tasks = function (tasks)
+        display_today_tasks = function(tasks)
             local name = "Today's Tasks"
             local res = {
                 "* " .. name,
-                ""
+                "",
             }
-            for _, task in ipairs(tasks) do
-                table.insert(res, "- " .. task)
+            local today_task = function(task)
+                if not task.contexts then
+                    return false
+                end
+                return vim.tbl_contains(task.contexts, "today")
             end
+
+            local today_tasks = vim.tbl_filter(today_task, tasks)
+
+            for _, t in pairs(today_tasks) do
+                local content = "- " .. t.content
+                table.insert(res, content)
+            end
+
             local buf = module.required["core.ui"].create_norg_buffer(name, "vsplitr")
             vim.api.nvim_buf_set_lines(buf, 0, -1, false, res)
             vim.api.nvim_buf_set_option(buf, "modifiable", false)
         end,
 
-        display_waiting_for = function (tasks)
+        display_waiting_for = function(tasks)
             local name = "Waiting For Tasks"
             local res = {
                 "* " .. name,
                 "",
             }
+            local waiting_for_tasks = module.private.sort_by("waiting_for", tasks)
+            waiting_for_tasks["_"] = nil
 
-            for waiting_for_name, waiting_for_tasks in pairs(tasks) do
-                table.insert(res, "** " .. waiting_for_name)
-                for _,t in pairs(waiting_for_tasks) do
-                    table.insert(res, "- " .. t)
+            for w, w_tasks in pairs(waiting_for_tasks) do
+                table.insert(res, "** " .. w)
+                for _, t in pairs(w_tasks) do
+                    local content = "- " .. t.content
+                    table.insert(res, content)
                 end
                 table.insert(res, "")
             end
@@ -34,17 +48,19 @@ return function (module)
             vim.api.nvim_buf_set_option(buf, "modifiable", false)
         end,
 
-        display_contexts = function (tasks)
+        display_contexts = function(tasks)
             local name = "Contexts"
             local res = {
                 "* " .. name,
-                ""
+                "",
             }
 
-            for context, context_tasks in pairs(tasks) do
+            local contexts_tasks = module.private.sort_by("contexts", tasks)
+
+            for context, c_tasks in pairs(contexts_tasks) do
                 table.insert(res, "** " .. context)
-                for _,t in pairs(context_tasks) do
-                    table.insert(res, "- " .. t)
+                for _, t in pairs(c_tasks) do
+                    table.insert(res, "- " .. t.content)
                 end
                 table.insert(res, "")
             end
@@ -52,6 +68,6 @@ return function (module)
             local buf = module.required["core.ui"].create_norg_buffer(name, "vsplitr")
             vim.api.nvim_buf_set_lines(buf, 0, -1, false, res)
             vim.api.nvim_buf_set_option(buf, "modifiable", false)
-        end
+        end,
     }
 end
--- lua/neorg/modules/core/gtd/base/gtd_queries.lua
+++ lua/neorg/modules/core/gtd/base/gtd_queries.lua
@@ -72,8 +72,6 @@ return function(module)
         --- @param state string
         --- @param opts table
         ---   - opts.filename (string):         will restrict the search only for the filename provided
-        ---   - opts.recursive (bool):          if true will search todos recursively in the AST
-        ---   - opts.extract (bool):            if false will return the nodes instead of the extracted content
         ---   - opts.exclude_files (table):     will exclude files from workspace in querying information
         --- @return table
         get_tasks = function(state, opts)
@@ -92,14 +90,14 @@ return function(module)
                     subtree = {
                         {
                             query = { "all", "generic_list" },
-                            recursive = opts.recursive,
+                            recursive = true,
                             subtree = {
                                 {
                                     query = { "all", "todo_item1" },
                                     where = where_statement,
-                                    subtree = {
-                                        { query = { "first", "paragraph" } },
-                                    },
+                                    --subtree = {
+                                    --{ query = { "first", "paragraph" } },
+                                    --},
                                 },
                             },
                         },
@@ -127,214 +125,156 @@ return function(module)
 
             for _, bufnr in pairs(bufnrs) do
                 local nodes = module.required["core.queries.native"].query_nodes_from_buf(tree, bufnr)
-
-                if opts.extract == false then
-                    vim.list_extend(res, nodes)
-                else
-                    local extracted = module.required["core.queries.native"].extract_nodes(nodes)
-                    vim.list_extend(res, extracted)
-                end
+                vim.list_extend(res, nodes)
             end
+
             return res
+        end,
+
+        add_metadata = function(task_nodes)
+            local res = {}
+
+            for _, task_node in pairs(task_nodes) do
+                local task = {}
+                task.task_node = task_node[1]
+                task.bufnr = task_node[2]
+
+                task.content = module.private.get_task_content(task)
+                task.project = module.private.get_task_project(task)
+                task.contexts = module.private.get_task_tag("contexts", task)
+                task.start = module.private.get_task_tag("time.start", task)
+                task.due = module.private.get_task_tag("time.due", task)
+                task.waiting_for = module.private.get_task_tag("waiting.for", task)
+
+                table.insert(res, task)
+            end
+
+            return res
+        end,
+
+        get_task_content = function(task)
+            local tree = {
+                { query = { "first", "paragraph" } },
+            }
+            local task_content = module.required["core.queries.native"].query_from_tree(
+                task.task_node,
+                tree,
+                task.bufnr
+            )
+
+            if #task_content == 0 then
+                return {}
+            end
+
+            local extracted = module.required["core.queries.native"].extract_nodes(task_content)
+            return extracted[1]
+        end,
+
+        get_task_project = function(task)
+            local project_node = module.required["core.queries.native"].find_parent_node(
+                { task.task_node, task.bufnr },
+                "heading1"
+            )
+
+            if not project_node[1] then
+                return nil
+            end
+
+            local tree = {
+                { query = { "all", "paragraph_segment" } },
+            }
+
+            local project_content_node = module.required["core.queries.native"].query_from_tree(
+                project_node[1],
+                tree,
+                project_node[2]
+            )
+
+            local extracted = module.required["core.queries.native"].extract_nodes(project_content_node)
+            return extracted[1]
+        end,
+
+        get_task_tag = function(tag_name, task, opts)
+            if not vim.tbl_contains({ "time.due", "time.start", "contexts", "waiting.for" }, tag_name) then
+                log.error("Please specify time.due|time.start|contexts|waiting.for in get_task_date function")
+                return
+            end
+
+            local tags_node = module.required["core.queries.native"].find_parent_node(
+                { task.task_node, task.bufnr },
+                "carryover_tag_set"
+            )
+            if not tags_node[1] then
+                return nil
+            end
+
+            local tree = {
+                {
+                    query = { "first", "carryover_tag" },
+                    where = { "child_content", "tag_name", tag_name },
+                    subtree = {
+                        {
+                            query = { "all", "tag_parameters" },
+                            subtree = {
+                                { query = { "all", "word" } },
+                            },
+                        },
+                    },
+                },
+            }
+
+            local tag_content_nodes = module.required["core.queries.native"].query_from_tree(
+                tags_node[1],
+                tree,
+                tags_node[2]
+            )
+
+            if #tag_content_nodes == 0 then
+                return nil
+            end
+
+            local extracted = module.required["core.queries.native"].extract_nodes(tag_content_nodes)
+
+            return extracted
         end,
 
         remove_from_table = function(t, el)
             for i, v in ipairs(t) do
                 if v == el then
-                    local removed = table.remove(t, i)
+                    table.remove(t, i)
                     break
                 end
             end
             return t
         end,
 
-        --- Sort `nodes` by projects.
-        --- The returned table is of type { [project_name] = [nodes|extracted_nodes] }
-        --- @param nodes table
-        --- @param opts table
-        ---   - opts.extract (bool):        if false will return the nodes instead of the extracted content
-        --- @return table
-        sort_by_project = function(nodes, opts)
-            opts = opts or {}
+        sort_by = function(sorter, tasks)
+            if not vim.tbl_contains({ "waiting_for", "contexts", "project" }, sorter) then
+                log.error("Please provide a correct sorter.")
+                return
+            end
             local res = {}
-            for _, node in pairs(nodes) do
-                local project = module.required["core.queries.native"].find_parent_node(node, "heading1")
-                if project[1] then
-                    local tree = {
-                        { query = { "all", "paragraph_segment" } },
-                    }
-                    local project_content = module.required["core.queries.native"].query_from_tree(
-                        project[1],
-                        tree,
-                        project[2]
-                    )
-                    local extracted = module.required["core.queries.native"].extract_nodes(project_content)
-                    if not res[extracted[1]] then
-                        res[extracted[1]] = {}
-                    end
-                    table.insert(res[extracted[1]], node)
+
+            local insert = function(t, k, v)
+                if not t[k] then
+                    t[k] = {}
+                end
+                table.insert(t[k], v)
+            end
+
+            for _, t in pairs(tasks) do
+                if not t[sorter] then
+                    insert(res, "_", t)
                 else
-                    if not res["_"] then
-                        res["_"] = {}
-                    end
-                    table.insert(res["_"], node)
-                end
-            end
-
-            if opts.extract == false then
-                return res
-            end
-
-            res = vim.tbl_map(module.required["core.queries.native"].extract_nodes, res)
-
-            return res
-        end,
-
-        --- Sort `nodes` by contexts.
-        --- The returned table is of type { [context_name] = [nodes|extracted_nodes] }
-        --- @param nodes table
-        --- @param opts table
-        ---   - opts.extract (bool):        if false will return the nodes instead of the extracted content
-        --- @return table
-        sort_by_context = function(nodes, opts)
-            opts = opts or {}
-            local res = {}
-            local insert_node_at_void = function(node, t)
-                if not t["_"] then
-                    t["_"] = {}
-                end
-                table.insert(t["_"], node)
-                return t
-            end
-
-            for _, node in pairs(nodes) do
-                -- Find the first parent node that match carryover_tag_set
-                local tags_node = module.required["core.queries.native"].find_parent_node(node, "carryover_tag_set")
-
-                -- There's no tag in one of parent's node of the current node
-                if not tags_node[1] then
-                    insert_node_at_void(node, res)
-                else
-                    local tree = {
-                        {
-                            query = { "all", "carryover_tag" },
-                            where = { "child_content", "tag_name", "contexts" },
-                            subtree = {
-                                {
-                                    query = { "all", "tag_parameters" },
-                                    subtree = {
-                                        { query = { "all", "word" } },
-                                    },
-                                },
-                            },
-                        },
-                    }
-
-                    local contexts = module.required["core.queries.native"].query_from_tree(
-                        tags_node[1],
-                        tree,
-                        tags_node[2]
-                    )
-
-                    if #contexts == 0 then
-                        insert_node_at_void(node, res)
-                    else
-                        local extracted = module.required["core.queries.native"].extract_nodes(contexts)
-
-                        for _, extracted_context in pairs(extracted) do
-                            if not res[extracted_context] then
-                                res[extracted_context] = {}
-                            end
-
-                            table.insert(res[extracted_context], node)
+                    if type(t[sorter]) == "table" then
+                        for _, s in pairs(t[sorter]) do
+                            insert(res, s, t)
                         end
+                    elseif type(t[sorter]) == "string" then
+                        insert(res, t[sorter], t)
                     end
                 end
             end
-
-            if opts.extract == false then
-                return res
-            end
-
-            res = vim.tbl_map(module.required["core.queries.native"].extract_nodes, res)
-
-            return res
-        end,
-
-        --- Filter `nodes` by today tasks
-        --- @param nodes userdata
-        --- @param opts table
-        ---   - opts.extract (bool):        if false will return the nodes instead of the extracted content
-        --- @return table
-        filter_today = function(nodes, opts)
-            opts = opts or {}
-            local today_tag = "today"
-            local nodes_by_context = module.private.sort_by_context(nodes, { extract = false })
-
-            if not nodes_by_context[today_tag] then
-                nodes_by_context[today_tag] = {}
-            end
-
-            if opts.extract == false then
-                return nodes_by_context[today_tag]
-            end
-
-            local today_tasks = module.required["core.queries.native"].extract_nodes(nodes_by_context[today_tag])
-            return today_tasks
-        end,
-
-        --- Filter tasks `nodes` with `tag_name`
-        --- @param nodes table
-        --- @param opts table
-        ---   - opts.extract (bool):        if false will return the nodes instead of the extracted content
-        --- @return table
-        filter_tags = function(nodes, tag_name, opts)
-            opts = opts or {}
-            local res = {}
-
-            for _, node in pairs(nodes) do
-                -- Find the first parent node that match carryover_tag_set
-                local tags_node = module.required["core.queries.native"].find_parent_node(node, "carryover_tag_set")
-
-                local tree = {
-                    {
-                        query = { "all", "carryover_tag" },
-                        where = { "child_content", "tag_name", tag_name },
-                        subtree = {
-                            {
-                                query = { "all", "tag_parameters" },
-                                subtree = {
-                                    { query = { "all", "word" } },
-                                },
-                            },
-                        },
-                    },
-                }
-
-                local found_tags = module.required["core.queries.native"].query_from_tree(
-                    tags_node[1],
-                    tree,
-                    tags_node[2]
-                )
-
-                if #found_tags ~= 0 then
-                    local extracted = module.required["core.queries.native"].extract_nodes(found_tags)
-
-                    for _, extracted_context in pairs(extracted) do
-                        if not res[extracted_context] then
-                            res[extracted_context] = {}
-                        end
-
-                        table.insert(res[extracted_context], node)
-                    end
-                end
-            end
-
-            if opts.extract == false then
-                return res
-            end
-
-            res = vim.tbl_map(module.required["core.queries.native"].extract_nodes, res)
 
             return res
         end,
