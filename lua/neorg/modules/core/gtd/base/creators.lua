c987cb9ecc75c06717bba6b59d68bcf99f66a7bd
Daniel Mathiot <d.danymat@gmail.com>
2021-09-10 14:36:43 +0200

    Update custom creator signature
    Former-commit-id: f4db3f277b63326edd9adf41360d880fc61985c4

--- lua/neorg/modules/core/gtd/base/creators.lua
+++ lua/neorg/modules/core/gtd/base/creators.lua
@@ -2,48 +2,45 @@ local ts_utils = require("nvim-treesitter.ts_utils")
 
 return function(module)
     return {
-        --- Creates a new project from the `project` table and insert it in `file`
-        --- @param project table
+        --- Creates a new project from the `project` table and insert it in `bufnr` at `location`
+        --- @param type string
+        --- @param node table
+        --- @param bufnr number
         ---   - project.content (string):         Mandatory field. It's the project name
         ---   - project.contexts (string[]):      Contexts names
         ---   - project.start (string):           Start date
         ---   - project.due (string):             Due date
         ---   - project.waiting_for (string[]):   Waiting For names
-        --- @param file string
-        create_project = function(project, file)
-            local files = module.required["core.norg.dirman"].get_norg_files(module.config.public.workspace)
-            if not vim.tbl_contains(files, file) then
-                log.error("File " .. file .. " is not from gtd workspace")
+        create = function(type, node, bufnr, location)
+            if not vim.tbl_contains({ "project", "task" }, type) then
+                log.error("You can only insert new project or task")
                 return
             end
-
-            if vim.tbl_count(project) == 0 or not project.content then
-                log.error("No project provided")
-                return
-            end
-
-            local bufnr = module.private.get_bufnr_from_file(file)
-            local tree = {
-                { query = { "first", "document_content" } },
-            }
-            local document = module.required["core.queries.native"].query_nodes_from_buf(tree, bufnr)[1]
-
-            local _, _, end_col, _ = ts_utils.get_node_range(document[1])
 
             local res = {}
 
-            table.insert(res, "")
-            module.private.insert_content(res, project.contexts, "$contexts")
-            module.private.insert_content(res, project.start, "$start")
-            module.private.insert_content(res, project.due, "$due")
-            module.private.insert_content(res, project.waiting_for, "$waiting.for")
-            module.private.insert_content(res, project.content, "*")
+            if not node.content then
+                log.error("No node content provided")
+                return
+            end
 
-            vim.api.nvim_buf_set_lines(bufnr, end_col, -1, false, res)
+            table.insert(res, "")
+            module.private.insert_content(res, node.contexts, "$contexts")
+            module.private.insert_content(res, node.start, "$start")
+            module.private.insert_content(res, node.due, "$due")
+            module.private.insert_content(res, node.waiting_for, "$waiting.for")
+            module.private.insert_content(res, node.content, type == "project" and "*" or "- [ ]")
+
+            vim.api.nvim_buf_set_lines(bufnr, location, location + #res, false, res)
             vim.api.nvim_buf_call(bufnr, function()
                 vim.cmd([[ write ]])
             end)
         end,
+
+        --- Creates a new task from `task` table and insert it
+        --- @param task table
+        --- @param opts table
+        create_task = function(task, opts) end,
 
         --- Insert formatted `content` in `t`, with `prefix` before it. Mutates `t` !
         --- @param t table
@@ -62,6 +59,27 @@ return function(module)
                 end
                 table.insert(t, inserted)
             end
+        end,
+
+        --- Returns the end of the document content position of a `file` and the `file` bufnr
+        --- @param file string
+        --- @return number
+        get_end_document_content = function(file)
+            local files = module.required["core.norg.dirman"].get_norg_files(module.config.public.workspace)
+            if not vim.tbl_contains(files, file) then
+                log.error("File " .. file .. " is not from gtd workspace")
+                return
+            end
+
+            local bufnr = module.private.get_bufnr_from_file(file)
+            local tree = {
+                { query = { "first", "document_content" } },
+            }
+            local document = module.required["core.queries.native"].query_nodes_from_buf(tree, bufnr)[1]
+
+            local _, _, end_row, _ = ts_utils.get_node_range(document[1])
+
+            return end_row, bufnr
         end,
     }
 end
