2ad250e1d88699e2bacccae996fdb8ac19462fa3
Daniel Mathiot <d.danymat@gmail.com>
2021-09-11 21:47:00 +0200

    Move GTD selection popup in core.gtd.ui
    Former-commit-id: 564525816a7726c8b551c69661a2189ed681ecf0

--- lua/neorg/modules/core/gtd/base/module.lua
+++ lua/neorg/modules/core/gtd/base/module.lua
@@ -12,7 +12,7 @@ REQUIRES:
     This module requires:
         - core.norg.dirman in order to get full path to the workspace
         - core.keybinds (check KEYBINDS for usage)
-        - core.ui in order to ask for user input
+        - core.gtd.ui for gtd UI components
         - core.neorgcmd to add commands capabilities
         - core.queries.native to fetch content from norg files
         - core.integrations.treesitter to use ts_utils
@@ -26,8 +26,6 @@ require("neorg.modules.base")
 require("neorg.events")
 
 local module = neorg.modules.create("core.gtd.base")
-local utils = require("neorg.external.helpers")
-local log = require("neorg.external.log")
 
 module.setup = function()
     return {
@@ -36,7 +34,6 @@ module.setup = function()
             "core.norg.dirman",
             "core.keybinds",
             "core.gtd.ui",
-            "core.ui",
             "core.gtd.queries",
             "core.neorgcmd",
         },
@@ -110,83 +107,7 @@ module.on_event = function(event)
                 module.config.public.default_lists.inbox
             )
         elseif event.split_type[2] == "gtd.quick_actions" then
-            module.required["core.ui"].create_selection("Quick actions", {
-                flags = {
-                    { "a", "Add a task to inbox" },
-                    {
-                        "l",
-                        {
-                            name = "List files",
-                            flags = {
-                                { "i", "Inbox" },
-                            },
-                        },
-                    },
-                    {},
-                    { "Test Queries (index.norg) file", "TSComment" },
-                    { "x", "testing" },
-                    { "p", "Projects" },
-                    {
-                        "t",
-                        {
-                            name = "Tasks",
-                            flags = {
-                                { "t", "Today tasks" },
-                                { "c", "contexts" },
-                                { "w", "Waiting for" },
-                                { "d", "Due tasks", true },
-                                { "s", "Start tasks", true },
-                            },
-                        },
-                    },
-                },
-            }, function(choices)
-                local tasks = module.required["core.gtd.queries"].get(
-                    "tasks",
-                    { exclude_files = module.config.public.exclude }
-                )
-                local projects = module.required["core.gtd.queries"].get(
-                    "projects",
-                    { exclude_files = module.config.public.exclude }
-                )
-                tasks = module.required["core.gtd.queries"].add_metadata(tasks, "task")
-                projects = module.required["core.gtd.queries"].add_metadata(projects, "project")
-
-                if choices[1] == "a" then
-                    module.required["core.gtd.ui"].add_task_to_inbox()
-                elseif choices[1] == "l" and choices[2] == "i" then
-                    module.required["core.norg.dirman"].open_file(
-                        module.config.public.workspace,
-                        module.config.public.default_lists.inbox
-                    )
-                elseif choices[1] == "p" then
-                    module.required["core.gtd.ui"].display_projects(tasks, projects, { priority = { "_" } })
-                elseif choices[1] == "t" then
-                    if choices[2] == "t" then
-                        module.required["core.gtd.ui"].display_today_tasks(tasks)
-                    elseif choices[2] == "w" then
-                        module.required["core.gtd.ui"].display_waiting_for(tasks)
-                    elseif choices[2] == "s" then
-                        log.warn(tasks)
-                    elseif choices[2] == "d" then
-                        log.warn(tasks)
-                    elseif choices[2] == "c" then
-                        module.required["core.gtd.ui"].display_contexts(
-                            tasks,
-                            { exclude = { "someday" }, priority = { "_" } }
-                        )
-                    end
-                elseif choices[1] == "x" then
-                    local end_row, bufnr = module.required["core.gtd.queries"].get_end_document_content("index.norg")
-                    module.required["core.gtd.queries"].create("project", {
-                        content = "This is a test",
-                        contexts = { "today", "someday" },
-                        start = "2021-12-22",
-                        due = "2021-12-23",
-                        waiting_for = { "vhyrro" },
-                    }, bufnr, end_row)
-                end
-            end)
+            module.required["core.gtd.ui"].show_quick_actions(module.config.public)
         end
     end
 end
