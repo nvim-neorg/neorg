efd00765e3b6fc196c46199acf859a255cef34f5
Daniel Mathiot <d.danymat@gmail.com>
2021-09-20 15:09:03 +0200

    Add support for due dates in quick actions

--- lua/neorg/modules/core/gtd/ui/add_to_inbox.lua
+++ lua/neorg/modules/core/gtd/ui/add_to_inbox.lua
@@ -1,11 +1,41 @@
 return function(module)
     return {
         public = {
+            -- @Summary Convert a date from text to YY-MM-dd format
+            -- @Description If the date is a quick capture (like 2w, 10d, 4m), it will convert to a standardized date
+            -- Supported formats ($ treated as number):
+            --   - $d: days from now (e.g 2d is 2 days from now)
+            --   - $w: weeks from now (e.g 2w is 2 weeks from now)
+            --   - $m: months from now (e.g 2m is 2 months from now)
+            --   - tomorrow: tomorrow's date
+            --   - today: today's date
+            --   The format for date is YY-mm-dd
+            -- @Param  text (string) the text to use
+            date_converter = function(text)
+                -- Get today's date
+                local now = os.date("%Y-%m-%d")
+                local y, m, d = now:match("(%d+)-(%d+)-(%d+)")
 
-            --- Add user task to inbox
-            add_task_to_inbox = function(task)
-                local configs = neorg.modules.get_module_config("core.gtd.base")
-                module.private.add_to_list(configs.default_lists.inbox, task)
+                -- Cases for converting quick dates to full dates (e.g 1w is one week from now)
+                local converted_date
+                local patterns = { weeks = "[%d]+w", days = "[%d]+d", months = "[%d]+m" }
+                local days_matched = text:match(patterns.days)
+                local weeks_matched = text:match(patterns.weeks)
+                local months_matched = text:match(patterns.months)
+                if text == "tomorrow" then
+                    converted_date = os.time({ year = y, month = m, day = d + 1 })
+                elseif text == "today" then
+                    return now
+                elseif weeks_matched ~= nil then
+                    converted_date = os.time({ year = y, month = m, day = d + 7 * weeks_matched:sub(1, -2) })
+                elseif days_matched ~= nil then
+                    converted_date = os.time({ year = y, month = m, day = d + days_matched:sub(1, -2) })
+                elseif months_matched ~= nil then
+                    converted_date = os.time({ year = y, month = m + months_matched:sub(1, -2), day = d })
+                else
+                    return nil
+                end
+                return os.date("%Y-%m-%d", converted_date)
             end,
         },
 
@@ -118,44 +148,7 @@ return function(module)
                 end
                 return content
             end,
-
-            -- @Summary Convert a date from text to YY-MM-dd format
-            -- @Description If the date is a quick capture (like 2w, 10d, 4m), it will convert to a standardized date
-            -- Supported formats ($ treated as number):
-            --   - $d: days from now (e.g 2d is 2 days from now)
-            --   - $w: weeks from now (e.g 2w is 2 weeks from now)
-            --   - $m: months from now (e.g 2m is 2 months from now)
-            --   - tomorrow: tomorrow's date
-            --   - today: today's date
-            --   The format for date is YY-mm-dd
-            -- @Param  text (string) the text to use
-            date_converter = function(text)
-                -- Get today's date
-                local now = os.date("%Y-%m-%d")
-                local y, m, d = now:match("(%d+)-(%d+)-(%d+)")
-
-                -- Cases for converting quick dates to full dates (e.g 1w is one week from now)
-                local converted_date
-                local patterns = { weeks = "[%d]+w", days = "[%d]+d", months = "[%d]+m" }
-                local days_matched = text:match(patterns.days)
-                local weeks_matched = text:match(patterns.weeks)
-                local months_matched = text:match(patterns.months)
-                if text == "tomorrow" then
-                    converted_date = os.time({ year = y, month = m, day = d + 1 })
-                elseif text == "today" then
-                    return now
-                elseif weeks_matched ~= nil then
-                    converted_date = os.time({ year = y, month = m, day = d + 7 * weeks_matched:sub(1, -2) })
-                elseif days_matched ~= nil then
-                    converted_date = os.time({ year = y, month = m, day = d + days_matched:sub(1, -2) })
-                elseif months_matched ~= nil then
-                    converted_date = os.time({ year = y, month = m + months_matched:sub(1, -2), day = d })
-                else
-                    return text
-                end
-                return os.date("%Y-%m-%d", converted_date)
-            end,
-
+            --
             ---Use the table_output in order to arrange the syntax field as a string
             ---@param syntax_type table one of the syntaxes of module.private.syntax
             ---@param tbl_output table the table to arrange
