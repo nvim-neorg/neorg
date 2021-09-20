ef2178292bf8282a3379219fb78557c5ee04a973
Daniel Mathiot <d.danymat@gmail.com>
2021-09-20 14:30:36 +0200

    Use concat function from selection popup in quick actions

--- lua/neorg/modules/core/gtd/ui/selection_popups.lua
+++ lua/neorg/modules/core/gtd/ui/selection_popups.lua
@@ -2,15 +2,6 @@ return function(module)
     return {
         public = {
             show_quick_actions = function(configs)
-                -- Get tasks and projects
-                local tasks = module.required["core.gtd.queries"].get("tasks", { exclude_files = configs.exclude })
-                local projects = module.required["core.gtd.queries"].get(
-                    "projects",
-                    { exclude_files = configs.exclude }
-                )
-                tasks = module.required["core.gtd.queries"].add_metadata(tasks, "task")
-                projects = module.required["core.gtd.queries"].add_metadata(projects, "project")
-
                 -- Generate quick_actions selection popup
                 local buffer = module.required["core.ui"].create_split("Quick Actions")
                 local selection = module.required["core.ui"].begin_selection(buffer)
@@ -24,60 +15,82 @@ return function(module)
                     :title("This is a text")
                     :blank()
                     :text("Capture")
-                    :flag("a", "Add a task to inbox", {
-                        callback = function()
-                            selection:push_page()
-
-                            selection:title("Add a task to inbox"):blank():prompt("Task", {
-                                callback = function(text)
-                                    local task = {}
-                                    task.content = text
-                                    selection:push_page()
-
-                                    selection
-                                        :title("Hey")
-                                        :blank()
-                                        :text("Task: " .. task.content)
-                                        :blank()
-                                        :flag("c", "Add Contexts", {
-                                            destroy = false,
-                                            callback = function()
-                                                selection:push_page()
-                                                selection
-                                                    :title("Add contexts")
-                                                    :text("Separate contexts with space")
-                                                    :blank()
-                                                    :prompt(">", {
-                                                        callback = function(text)
-                                                            if #text > 0 then
-                                                                task.contexts = task.contexts or {}
-                                                                task.contexts = vim.list_extend(
-                                                                    task.contexts,
-                                                                    vim.split(text, " ", false)
-                                                                )
-                                                            end
-                                                        end,
-                                                        pop = true,
-                                                    })
-                                            end,
-                                        })
-                                        :flag("f", "Finish", function()
-                                            local end_row, bufnr =
-                                                module.required["core.gtd.queries"].get_end_document_content(
-                                                    "inbox.norg"
-                                                )
-                                            module.required["core.gtd.queries"].create("task", task, bufnr, end_row)
-                                        end)
-                                end,
-                                -- Do not pop or destroy the prompt when confirmed
-                                pop = false,
-                                destroy = false,
-                            })
-                        end,
-                        destroy = false,
-                    })
+                    :concat(module.private.add_to_inbox)
                     :blank()
                     :text("Displays")
+                    :concat(function(_selection)
+                        module.private.generate_display_flags(_selection, configs)
+                    end)
+            end,
+        },
+        private = {
+            add_to_inbox = function(selection)
+                selection:flag("a", "Add a task to inbox", {
+                    callback = function()
+                        selection:push_page()
+
+                        selection:title("Add a task to inbox"):blank():prompt("Task", {
+                            callback = function(text)
+                                local task = {}
+                                task.content = text
+                                selection:push_page()
+
+                                selection
+                                    :title("Hey")
+                                    :blank()
+                                    :text("Task: " .. task.content)
+                                    :blank()
+                                    :flag("c", "Add Contexts", {
+                                        destroy = false,
+                                        callback = function()
+                                            selection:push_page()
+                                            selection
+                                                :title("Add contexts")
+                                                :text("Separate contexts with space")
+                                                :blank()
+                                                :prompt(">", {
+                                                    callback = function(text)
+                                                        if #text > 0 then
+                                                            task.contexts = task.contexts or {}
+                                                            task.contexts = vim.list_extend(
+                                                                task.contexts,
+                                                                vim.split(text, " ", false)
+                                                            )
+                                                        end
+                                                    end,
+                                                    pop = true,
+                                                })
+                                        end,
+                                    })
+                                    :flag("f", "Finish", function()
+                                        local end_row, bufnr =
+                                            module.required["core.gtd.queries"].get_end_document_content(
+                                                "inbox.norg"
+                                            )
+                                        module.required["core.gtd.queries"].create("task", task, bufnr, end_row)
+                                    end)
+                            end,
+                            -- Do not pop or destroy the prompt when confirmed
+                            pop = false,
+                            destroy = false,
+                        })
+                    end,
+                    destroy = false,
+                })
+                return selection
+            end,
+
+            generate_display_flags = function(selection, configs)
+                -- Get tasks and projects
+                local tasks = module.required["core.gtd.queries"].get("tasks", { exclude_files = configs.exclude })
+                local projects = module.required["core.gtd.queries"].get(
+                    "projects",
+                    { exclude_files = configs.exclude }
+                )
+                tasks = module.required["core.gtd.queries"].add_metadata(tasks, "task")
+                projects = module.required["core.gtd.queries"].add_metadata(projects, "project")
+
+                selection
                     :flag("p", "Projects", function()
                         module.public.display_projects(tasks, projects, { priority = { "_" } })
                     end)
@@ -95,6 +108,7 @@ return function(module)
                                 module.public.display_waiting_for(tasks)
                             end)
                     end)
+                return selection
             end,
         },
     }
