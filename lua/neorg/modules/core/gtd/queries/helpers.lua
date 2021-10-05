a42564b2cb8c9e1298a80112ebbb8a3c5db98934
Daniel Mathiot <d.danymat@gmail.com>
2021-10-05 22:52:49 +0200

    (gtd.queries) Function to get a diff between two dates

--- lua/neorg/modules/core/gtd/queries/helpers.lua
+++ lua/neorg/modules/core/gtd/queries/helpers.lua
@@ -1,5 +1,9 @@
 local module = neorg.modules.extend("core.gtd.queries.helpers")
 
+--- @class dateDiff
+--- @field weeks number
+--- @field days number
+
 module.public = {
     -- @Summary Convert a date from text to YY-MM-dd format
     -- @Description If the date is a quick capture (like 2w, 10d, 4m), it will convert to a standardized date
@@ -37,6 +41,32 @@ module.public = {
         end
         return os.date("%Y-%m-%d", converted_date)
     end,
+
+    --- Parses a date string to table relative to today's date
+    --- (e.g { weeks = 2, days = 2, years= 0 } for in 2 weeks and 2 days)
+    --- @param date string #A date formatted with YY-MM-dd format
+    --- @return dateDiff
+    diff_with_today = function(date)
+        vim.validate({
+            date = { date, "string" },
+        })
+        -- Get today's date
+        local now = os.date("%Y-%m-%d")
+        local y_now, m_now, d_now = now:match("(%d+)-(%d+)-(%d+)")
+        local now_timestamp = os.time({ year = y_now, month = m_now, day = d_now })
+
+        -- Parse date parameter
+        local y, m, d = date:match("(%d+)-(%d+)-(%d+)")
+        local date_timestamp = os.time({ year = y, month = m, day = d })
+
+        -- Find out how many elapsed seconds between now and the date
+        local elapsed_seconds = date_timestamp - now_timestamp
+        elapsed_seconds = elapsed_seconds
+
+        local elapsed = module.private.convert_seconds(elapsed_seconds)
+
+        return elapsed
+    end,
 }
 
 module.private = {
@@ -57,6 +87,32 @@ module.private = {
             end
             table.insert(t, inserted)
         end
+    end,
+
+    --- Converts seconds to an actual table
+    --- @param seconds number
+    --- @return dateDiff
+    convert_seconds = function(seconds)
+        local negative_values = false
+        if seconds < 0 then
+            seconds = math.abs(seconds)
+            negative_values = true
+        end
+
+        local weeksDiff = math.floor(seconds / 604800)
+        local remainder = (seconds % 604800)
+        local daysDiff = math.floor(remainder / 86400)
+
+        if negative_values then
+            weeksDiff = weeksDiff ~= 0 and -weeksDiff or 0
+            daysDiff = daysDiff ~= 0 and -daysDiff or 0
+        end
+
+        local res = {
+            weeks = weeksDiff,
+            days = daysDiff,
+        }
+        return res
     end,
 }
 
