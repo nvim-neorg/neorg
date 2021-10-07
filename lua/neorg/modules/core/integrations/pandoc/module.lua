ae7b201005d9007b2a11e214884d5c6cc6db4a01
Daniel Mathiot <d.danymat@gmail.com>
2021-10-07 20:12:12 +0200

    (integrations.pandoc) Update pandoc types

--- lua/neorg/modules/core/integrations/pandoc/module.lua
+++ lua/neorg/modules/core/integrations/pandoc/module.lua
@@ -7,28 +7,6 @@ module.setup = function()
     return { success = true, requires = { "core.integrations.treesitter" } }
 end
 
-module.config.public = {
-    pandoc_conversion = {
-        -- break_as: the content table will be
-        ["paragraph"] = { type = "Para" },
-        ["heading1"] = {
-            subtree = {
-                ["paragraph_segment"] = { position = 1, type = "Header" },
-            },
-        },
-    },
-
-    pandoc_types = {
-        ["Header"] = { types = { "Int", "Attr", { "Inline" } } },
-        ["Para"] = { types = { { "Inline" } } },
-        ["Inline"] = { subtypes = { "Str", "Space" }, force = "Str" },
-        ["Str"] = "string",
-        ["Int"] = "number",
-        ["Attr"] = "string",
-        ["Space"] = "string",
-    },
-}
-
 module.public = {
     ---
     --- @param bufnr number
@@ -38,34 +16,63 @@ module.public = {
             bufnr = { bufnr, "number" },
             format = { format, "string" },
         })
-        local res = module.private.generate_pandoc(bufnr, format)
+        local res = module.private.generate_pandoc(bufnr)
 
         -- Requiring json encoder/decoder
         local json = require("neorg.modules.core.integrations.pandoc.json")
 
-        res = json.encode(res)
         P(res)
+        res = json.encode(res)
 
         -- Create file and start job
-        os.execute("echo '" .. res .. "' > /tmp/neorg_gen_pandoc")
-        Job
-            :new({
-                command = "pandoc",
-                args = { "-f", "json", "-t", format, "/tmp/neorg_gen_pandoc" },
-                on_stdout = function()
-                    os.remove("/tmp/neorg_gen_pandoc")
-                end,
-                on_stderr = function(_, return_val)
-                    log.error(return_val)
-                    os.remove("/tmp/neorg_gen_pandoc")
-                end,
-            })
-            :start()
+        -- os.execute("echo '" .. res .. "' > /tmp/neorg_gen_pandoc")
+        -- Job
+        --     :new({
+        --         command = "pandoc",
+        --         args = { "-f", "json", "-t", format, "/tmp/neorg_gen_pandoc" },
+        --         on_stdout = function()
+        --             os.remove("/tmp/neorg_gen_pandoc")
+        --         end,
+        --         on_stderr = function(_, return_val)
+        --             log.error(return_val)
+        --             os.remove("/tmp/neorg_gen_pandoc")
+        --         end,
+        --     })
+        --     :start()
     end,
 }
 
+module.config.public = {
+    pandoc_conversion = {
+        -- Binding between the TS AST and pandoc's AST
+        -- if the node is found in this list of nodes, it'll create a table in the current depth results like so:
+        -- {
+        --     t = type,
+        --     c = {} -- or will extract it's content if the type is a terminal type
+        -- }
+        -- - if override, subtypes will be overriden by the values provided and the recursiveness will stop at current depth.
+        --   - "s" is a special overriding word, which means: "Extract the current node text inside it"
+        -- - if child, the type will be matched by the child instead of the actual parent
+        ["paragraph"] = { type = "Para", override = { { "s" } } },
+        ["heading1"] = { type = "Header", child = "paragraph_segment", override = { 1, "s", { "s" } } },
+    },
+
+    pandoc_types = {
+        -- types (table):       provides the list of mandatory types
+        -- subtypes (table):    the pandoc type is comprised of those types (must pick one of those)
+        ["Header"] = { types = { "Int", "Attr", { "Inline" } } },
+        ["Para"] = { types = { { "Inline" } } },
+        ["Inline"] = { subtypes = { "Str", "Space" } },
+        -- Terminal types
+        ["Str"] = "string",
+        ["Int"] = "number",
+        ["Attr"] = "string",
+        ["Space"] = "string",
+    },
+}
+
 module.private = {
-    generate_pandoc = function(bufnr, format)
+    generate_pandoc = function(bufnr)
         local res = {}
         res["pandoc-api-version"] = { 1, 22 }
         res.meta = {}
@@ -78,36 +85,20 @@ module.private = {
 
         local neorg_types = vim.tbl_keys(module.config.public.pandoc_conversion)
 
+        local ref = res.blocks
         local function descend(node)
             for child, _ in node:iter_children() do
                 if vim.tbl_contains(neorg_types, child:type()) then
                     local generator = module.config.public.pandoc_conversion[child:type()]
 
-                    local content
-                    if generator.parent then
-                        -- Depends of his parent to export
-                        local parent = child:parent()
-                        if parent:type() == generator.parent then
-                            content = ts_utils.get_node_text(child, bufnr)[1]
-                        end
-
-                        local values
-
-                        if generator.break_as then
-                            values = { t = generator.break_as, c = content }
-                        else
-                            values = { content }
-                        end
-
-                        if generator.position then
-                            content = { generator.position, content, values }
-                        end
-                    else
-                        content = ts_utils.get_node_text(child, bufnr)[1]
-                    end
-
-                    table.insert(res.blocks, { t = generator.type, c = content })
+                    local content = ts_utils.get_node_text(child, bufnr)[1]
+                    local inserted = { t = generator.type, c = { content } }
+                    table.insert(ref, inserted)
+                    ref = ref[#ref].c
+                    descend(child)
                 end
+
+                -- Recursively constructs the table
                 descend(child)
             end
         end
