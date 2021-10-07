305052b7479d24e441c6efcb450648e84eff11e0
Vhyrro <vhyrro@gmail.com>
2021-10-07 20:09:52 +0200

    fix: issues with highlights, module loading not working at times
    Former-commit-id: dcb88e96a90b6c1f3b058d5461c9e65f2877e00a

--- lua/neorg/modules.lua
+++ lua/neorg/modules.lua
@@ -189,12 +189,16 @@ function neorg.modules.load_module(module_name, parent, config)
     end
 
     -- Attempt to require the module, does not throw an error if the module doesn't exist
-    local exists, module = pcall(require, "neorg.modules." .. module_name)
+    local exists, module = pcall(require, "neorg.modules." .. module_name .. ".module")
 
     -- If the module doesn't exist then return false
     if not exists then
-        log.error("Unable to load module", module_name, "-", module)
-        return false
+        exists, module = pcall(require, "neorg.modules." .. module_name)
+
+        if not exists then
+            log.error("Unable to load module", module_name, "-", module)
+            return false
+        end
     end
 
     -- If the module is nil for some reason return false
