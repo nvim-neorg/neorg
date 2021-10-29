1e580e98a76ee3c9dd9a25b556f590222aca185e
danymat <d.danymat@gmail.com>
2021-10-29 14:35:58 +0200

    test(core.gtd.queries) Add tests for add_metadata

--- tests/core/gtd/queries/core_gtd_queries_spec.lua
+++ tests/core/gtd/queries/core_gtd_queries_spec.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: undefined-global
+
 -- Generate a simple config file
 local path = vim.fn.getcwd()
 require("neorg").setup({
@@ -54,9 +56,62 @@ describe("GTD - Retrievers:", function()
     it("Verify the task types and bufnr", function()
         local tasks = queries.get("tasks", { bufnr = buf })
         for _, task in pairs(tasks) do
-            assert.equals("userdata", type(task[1]))
-            assert.equals("number", type(task[2]))
+            assert.is_userdata(task[1])
+            assert.is_number(task[2])
             assert.equals(buf, task[2])
+        end
+    end)
+
+    it("Add metadata to node task", function()
+        local _buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_name(_buf, "test.norg")
+        vim.api.nvim_buf_set_lines(_buf, 0, 0, false, {
+            "this is a test",
+            "- [ ] test1",
+            "- [ ] test2",
+            "- [x] test3",
+            "- [*] test4",
+            "#contexts test_context test_context2",
+            "#time.due 2021-10-10",
+            "#time.start 2021-10-11",
+            "#waiting.for test_waiting_for",
+            "- [ ] test5",
+        })
+        local tasks = queries.get("tasks", { bufnr = _buf })
+        tasks = queries.add_metadata(tasks, "task")
+
+        for i, task in ipairs(tasks) do
+            assert.equals(i, task.position)
+        end
+
+        assert.equals("test1", tasks[1].content)
+        assert.equals("test2", tasks[2].content)
+        assert.equals("test3", tasks[3].content)
+        assert.equals("test4", tasks[4].content)
+        assert.equals("test5", tasks[5].content)
+
+        assert.equals("undone", tasks[1].state)
+        assert.equals("undone", tasks[2].state)
+        assert.equals("done", tasks[3].state)
+        assert.equals("pending", tasks[4].state)
+        assert.equals("undone", tasks[5].state)
+
+        assert.equals(2, #tasks[5].contexts)
+        assert.is_true(vim.tbl_contains(tasks[5].contexts, "test_context"))
+        assert.is_true(vim.tbl_contains(tasks[5].contexts, "test_context2"))
+
+        assert.equals(1, #tasks[5]["time.due"])
+        assert.is_true(vim.tbl_contains(tasks[5]["time.due"], "2021-10-10"))
+        assert.equals(1, #tasks[5]["time.start"])
+        assert.is_true(vim.tbl_contains(tasks[5]["time.start"], "2021-10-11"))
+        assert.equals(1, #tasks[5]["waiting.for"])
+        assert.is_true(vim.tbl_contains(tasks[5]["waiting.for"], "test_waiting_for"))
+
+        for _, task in pairs(tasks) do
+            local keys = vim.tbl_keys(task)
+            assert.is_true(vim.tbl_contains(keys, "node"))
+            assert.is_true(vim.tbl_contains(keys, "bufnr"))
+            assert.is_true(vim.tbl_contains(keys, "content"))
         end
     end)
 end)
