0c6e54d9678c2ecf5d3b713e2c19bb0d293c2ed0
danymat <d.danymat@gmail.com>
2021-11-01 21:53:49 +0100

    ref(core.gtd.queries) Move function to private field
    Former-commit-id: 63f917ac1bd076708192511a1cfcbc303f1c4c9d

--- lua/neorg/modules/core/gtd/queries/modifiers.lua
+++ lua/neorg/modules/core/gtd/queries/modifiers.lua
@@ -40,7 +40,7 @@ module.public = {
                 log.error("Please specify a tag with opts.tag")
                 return
             end
-            module.public.insert_tag({ object.node, object.bufnr }, value, opts.tag)
+            module.private.insert_tag({ object.node, object.bufnr }, value, opts.tag)
             return module.public.update(object, node_type)
         end
 
@@ -53,7 +53,7 @@ module.public = {
                 -- Delete the tag and recreate it with new values
                 local line_nr
                 object, line_nr = module.public.delete(object, node_type, option)
-                module.public.insert_tag({ object.node, object.bufnr }, value, opts.tag, { line = line_nr })
+                module.private.insert_tag({ object.node, object.bufnr }, value, opts.tag, { line = line_nr })
                 return module.public.update(object, node_type)
             else
                 log.error("Only tags and content are supported for modification")
