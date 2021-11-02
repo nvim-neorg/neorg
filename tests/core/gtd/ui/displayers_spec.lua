e81a4ef9562235cbb2f2aea3bf7dec1e7227cefd
danymat <d.danymat@gmail.com>
2021-11-02 20:26:45 +0100

    test(core.gtd.ui) Add tests for today tasks

---
+++ tests/core/gtd/ui/displayers_spec.lua
@@ -0,0 +1,33 @@
+---@diagnostic disable: undefined-global
+local config = require("tests.config")
+
+-- Get the required module
+local queries = neorg.modules.get_module("core.gtd.ui")
+
+describe("CORE.GTD.QUERIES - Displayers:", function()
+    it("Displays today tasks", function()
+        local tasks = {
+            { content = "test_task", contexts = { "today", "mac" }, state = "undone" },
+            { content = "done_task", contexts = { "today", "home" }, state = "done" },
+            { content = "test_task2", contexts = { "today", "home" }, state = "undone" },
+        }
+        local buf = queries.display_today_tasks(tasks)
+        assert.is_number(buf)
+
+        local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+        assert.is_true(vim.tbl_contains(lines, "** mac"))
+        assert.is_true(vim.tbl_contains(lines, "** home"))
+
+        assert.is_false(vim.tbl_contains(lines, "- done_task"))
+        assert.is_true(vim.tbl_contains(lines, "- test_task"))
+        assert.is_true(vim.tbl_contains(lines, "- test_task2"))
+
+        assert.equals(1, #vim.tbl_filter(function(t)
+            return t == "- test_task2"
+        end, lines))
+        assert.equals(1, #vim.tbl_filter(function(t)
+            return t == "- test_task"
+        end, lines))
+    end)
+end)
