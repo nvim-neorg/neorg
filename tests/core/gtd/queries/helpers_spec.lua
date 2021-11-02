4b205d36ab47ce390003171567cefe7d928d58c4
danymat <d.danymat@gmail.com>
2021-11-02 09:05:28 +0100

    fix(core.gtd.queries) Better group matchings, add tests
    Former-commit-id: dd569d2f85c6552b4f19ab24e45996facb9b2fc4

--- tests/core/gtd/queries/helpers_spec.lua
+++ tests/core/gtd/queries/helpers_spec.lua
@@ -16,4 +16,27 @@ describe("CORE.GTD.QUERIES - Helpers:", function()
         assert.is_nil(date)
         -- TODO: Add more test cases for custom dates
     end)
+
+    it("Gets a diff between today's date", function()
+        local date = os.date("*t")
+
+        local time = os.time({ day = date.day + 3, year = date.year, month = date.month })
+        local tested_date = os.date("%Y-%m-%d", time)
+        local diff = queries.diff_with_today(tested_date)
+
+        assert.is_table(diff)
+        assert.equals(0, diff.weeks)
+        assert.equals(3, diff.days)
+
+        time = os.time({ day = date.day + 16, year = date.year, month = date.month })
+        tested_date = os.date("%Y-%m-%d", time)
+        diff = queries.diff_with_today(tested_date)
+
+        assert.is_table(diff)
+        assert.equals(2, diff.weeks)
+        assert.equals(2, diff.days)
+
+        diff = queries.diff_with_today("test_date")
+        assert.is_nil(diff)
+    end)
 end)
