1a13b00f0d28fb8f29c73f3d16debcdb487edee7
danymat <d.danymat@gmail.com>
2021-11-06 22:00:09 +0100

    feat(wiki) Create docgen script for wiki generation

--- lua/neorg/modules/core/autocommands/module.lua
+++ lua/neorg/modules/core/autocommands/module.lua
@@ -1,32 +1,39 @@
 --[[
---	AUTOCOMMAND MODULE FOR NEORG
---	This module exposes functionality for subscribing to autocommands and performing actions based on those autocommands
+   File: Autocommands
+# AUTOCOMMAND MODULE FOR NEORG
 
-USAGE:
+This module exposes functionality for subscribing to autocommands and performing actions based on those autocommands
 
-	In your module.setup(), make sure to require core.autocommands (requires = { "core.autocommands" })
-	Afterwards in a function of your choice that gets called *after* core.autocommmands gets intialized e.g. load():
+## USAGE
 
-	module.load = function()
-		module.required["core.autocommands"].enable_autocommand("VimLeavePre") -- Substitute VimLeavePre for any valid neovim autocommand
-	end
+In your `module.setup()`, make sure to require core.autocommands (requires = { "core.autocommands" })
+Afterwards in a function of your choice that gets called *after* core.autocommmands gets intialized e.g. load():
 
-	Afterwards, be sure to subscribe to the event:
+```lua
+module.load = function()
+    module.required["core.autocommands"].enable_autocommand("VimLeavePre") -- Substitute VimLeavePre for any valid neovim autocommand
+end
+```
 
-	module.events.subscribed = {
+Afterwards, be sure to subscribe to the event:
 
-		["core.autocommands"] = {
-			vimleavepre = true
-		}
+```lua
+module.events.subscribed = {
 
-	}
+    ["core.autocommands"] = {
+        vimleavepre = true
+    }
 
-	Upon receiving an event, it will come in this format:
-	{
-		type = "core.autocommands.events.<name of autocommand, e.g. vimleavepre>",
-		broadcast = true
-	}
+}
+```
 
+Upon receiving an event, it will come in this format:
+```lua
+{
+    type = "core.autocommands.events.<name of autocommand, e.g. vimleavepre>",
+    broadcast = true
+}
+```
 --]]
 
 require("neorg.modules.base")
