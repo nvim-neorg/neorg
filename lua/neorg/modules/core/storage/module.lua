78d48ffc2b13eb7a5fe0d1f382dcd4edb35b2a77
Vhyrro <vhyrro@gmail.com>
2021-12-08 20:36:35 +0100

    feat: add `core.storage`, a module for dealing with persistent storage
    Former-commit-id: 5cab76fd1d1b65c6790d8506d10bcfdeed32de6b

---
+++ lua/neorg/modules/core/storage/module.lua
@@ -0,0 +1,86 @@
+--[[
+    File: Storage
+    Title: Store persistent data and query it easily with `core.storage`
+    Summary: Deals with storing persistent data across Neorg sessions.
+    ---
+--]]
+
+require("neorg.modules.base")
+
+local module = neorg.modules.create("core.storage")
+
+module.setup = function()
+    return {
+        requires = {
+            "core.autocommands",
+        },
+    }
+end
+
+module.config.public = {
+    path = vim.fn.stdpath("data") .. "/neorg.json",
+}
+
+module.private = {
+    data = {},
+}
+
+module.public = {
+    sync = function()
+        local file = io.open(module.config.public.path, "r")
+
+        if not file then
+            return
+        end
+
+        local content = file:read("*a")
+
+        io.close(file)
+
+        module.private.data = vim.json.decode(content)
+    end,
+
+    store = function(key, data)
+        module.private.data[key] = data
+    end,
+
+    remove = function(key)
+        module.private[key] = nil
+    end,
+
+    retrieve = function(key)
+        return module.private.data[key]
+    end,
+
+    flush = function()
+        local file = io.open(module.config.public.path, "w")
+
+        if not file then
+            return
+        end
+
+        file:write(vim.json.encode(module.private.data))
+
+        io.close(file)
+    end,
+}
+
+module.on_event = function(event)
+    if event.type == "core.autocommands.events.vimleavepre" then
+        module.public.flush()
+    end
+end
+
+module.load = function()
+    module.required["core.autocommands"].enable_autocommand("VimLeavePre")
+
+    module.public.sync()
+end
+
+module.events.subscribed = {
+    ["core.autocommands"] = {
+        vimleavepre = true,
+    },
+}
+
+return module
