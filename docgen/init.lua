da15b886bb18a59a9de63c61184d9f5a676b6d88
danymat <d.danymat@gmail.com>
2021-12-09 11:03:33 +0100

    feat(docgen) Add support for _Sidebar
    Former-commit-id: 898160fccbad199e42bb95757de5ee65e8117b2e

--- docgen/init.lua
+++ docgen/init.lua
@@ -107,7 +107,6 @@ docgen.generate_md_file = function(buf, path, comment, main_page)
 
     local structure
     if main_page == "Home" then
-        -- TODO:
         structure = {
             "<div align='center'>",
             "# Welcome to the neorg wiki !",
@@ -156,8 +155,46 @@ docgen.generate_md_file = function(buf, path, comment, main_page)
     elseif main_page == "_Sidebar" then
         structure = {
             "<div align='center'>",
+            "",
             "# :star2: Neorg",
             "</div>",
+            "",
+            "### Setting Up",
+            "- [Installation Guide](https://github.com/vhyrro/neorg/wiki/Installation)",
+            "- [How do I configure modules?](https://github.com/vhyrro/neorg/wiki/Configuring-Modules)",
+            "- [User Keybinds](https://github.com/vhyrro/neorg/wiki/User-Keybinds)",
+            "- [User Callbacks](https://github.com/vhyrro/neorg/wiki/User-Callbacks)",
+            "- [Modifying Neorg Highlights](https://github.com/vhyrro/neorg/wiki/Custom-Highlights)",
+            "- [Customizing Icons](https://github.com/vhyrro/neorg/wiki/Concealing)",
+            "### Usage",
+            "- [Managing Workspaces](https://github.com/vhyrro/neorg/wiki/Workspace-Management)",
+            "- [Constructing a Workflow](https://github.com/vhyrro/neorg/wiki/Constructing-a-Workflow)",
+            "### For the programmer",
+            "- [Writing my own module](https://github.com/vhyrro/neorg/wiki/Creating-Modules)",
+            "- [Hotswapping modules](https://github.com/vhyrro/neorg/wiki/Hotswapping-Modules)",
+            "- [Difference between module.public and module.config.public](https://github.com/vhyrro/neorg/wiki/Public-vs-Public-Config)",
+            "- [Metamodules](https://github.com/vhyrro/neorg/wiki/Metamodules)",
+            "- Inbuilt modules:",
+            function()
+                local res = {}
+                -- P(modules)
+                for _module, _config in pairs(modules) do
+                    local insert = "\t"
+                    if _config.filename then
+                        insert = insert
+                            .. "- [`"
+                            .. _config.name
+                            .. "`](https://github.com/nvim-neorg/neorg/wiki/"
+                            .. _config.filename
+                            .. ")"
+                    else
+                        insert = insert .. "- `" .. _module .. "`"
+                    end
+
+                    table.insert(res, insert)
+                end
+                return res
+            end,
         }
     else
         structure = {
