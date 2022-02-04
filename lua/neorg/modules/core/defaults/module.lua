fbccf4c57e8e947720db42349b96af582ba51a15
Vhyrro <vhyrro@gmail.com>
2022-02-04 16:38:47 +0100

    remove `core.norg.manoeuvre` from `core.defaults`

--- lua/neorg/modules/core/defaults/module.lua
+++ lua/neorg/modules/core/defaults/module.lua
@@ -20,6 +20,5 @@ return neorg.modules.create_meta(
     "core.norg.esupports.metagen",
     "core.norg.esupports.hop",
     "core.integrations.treesitter",
-    "core.norg.manoeuvre",
     "core.norg.news"
 )
