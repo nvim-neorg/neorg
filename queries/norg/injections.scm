dbdb21c8d186f9efa75cf423fbfe031d8b7d8bad
Vhyrro <vhyrro@gmail.com>
2022-02-12 19:27:17 +0100

    fix(injections): injections applied to the wrong tag type

--- queries/norg/injections.scm
+++ queries/norg/injections.scm
@@ -1,12 +1,14 @@
 ; Injection for code blocks
-(ranged_tag (tag_name) @_tagname (tag_parameters parameter: (tag_param) @language) (ranged_tag_content) @content (#eq? @_tagname "code") (#not-eq? @language "norg"))
-(ranged_tag (tag_name) @_tagname (tag_parameters)? (ranged_tag_content) @latex (#eq? @_tagname "math"))
+(ranged_verbatim_tag (tag_name) @_tagname (tag_parameters parameter: (tag_param) @language) (ranged_verbatim_tag_content) @content (#eq? @_tagname "code") (#not-eq? @language "norg"))
+(ranged_verbatim_tag (tag_name) @_tagname (tag_parameters)? (ranged_verbatim_tag_content) @latex (#eq? @_tagname "math"))
+
+; doesn't work rn: (ranged_tag (tag_name) @_tagname (tag_parameters)? (ranged_tag_content) @norg (#eq? @_tagname "example"))
 
 (
     (inline_math) @latex
     (#offset! @latex 0 1 0 -1)
 )
 
-(ranged_tag (tag_name) @_tagname (ranged_tag_content) @norg_meta (#eq? @_tagname "document.meta"))
+(ranged_verbatim_tag (tag_name) @_tagname (ranged_verbatim_tag_content) @norg_meta (#eq? @_tagname "document.meta"))
 
-(ranged_tag (tag_name) @_tagname (ranged_tag_content) @norg_table (#eq? @_tagname "table"))
+(ranged_verbatim_tag (tag_name) @_tagname (ranged_verbatim_tag_content) @norg_table (#eq? @_tagname "table"))
