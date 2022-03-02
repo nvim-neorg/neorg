2a593e12a0fe7b67ca507f73458d35773ac30b06
Vhyrro <vhyrro@gmail.com>
2022-03-02 15:35:17 +0100

    fix(dirman): expand paths relative to current file, not cwd (#391)

--- lua/neorg/modules/core/norg/dirman/module.lua
+++ lua/neorg/modules/core/norg/dirman/module.lua
@@ -472,22 +472,29 @@ module.public = {
 
     expand_path = function(path)
         -- Expand special chars like `$`
-        local workspace, custom_workspace_path = path:match("^($([^/]*))")
+        local custom_workspace_path = path:match("^%$([^/]*)/")
 
-        if custom_workspace_path and custom_workspace_path:len() > 0 then
-            local workspace_path = module.public.get_workspace(custom_workspace_path)
+        if custom_workspace_path then
+            -- If the user has given an empty workspace name (i.e. `$/myfile`)
+            if custom_workspace_path:len() == 0 then
+                path = module.public.get_current_workspace()[2] .. "/" .. path:sub(3)
+            else -- If the user provided a workspace name (i.e. `$my-workspace/myfile`)
+                local workspace_path = module.public.get_workspace(custom_workspace_path)
 
-            if not workspace_path then
-                log.trace("Unable to expand path: workspace does not exist")
-                return
+                if not workspace_path then
+                    log.warn("Unable to expand path: workspace does not exist")
+                    return
+                end
+
+                path = workspace_path .. "/" .. path:sub(custom_workspace_path:len() + 3)
             end
-
-            path = workspace_path .. workspace_path:sub(custom_workspace_path:len() + 2)
-        elseif workspace then
-            path = module.public.get_current_workspace()[2] .. path:sub(workspace:len() + 1)
+        else
+            -- If the path isn't absolute (doesn't begin with `/` nor `~`) then prepend the current file's
+            -- filehead in front of the path
+            path = (vim.tbl_contains({ "/", "~" }, path:sub(1, 1)) and "" or (vim.fn.expand("%:p:h") .. "/")) .. path
         end
 
-        return vim.fn.fnamemodify(path .. ".norg", ":p")
+        return path .. ".norg"
     end,
 
     --- Touches a file in workspace
