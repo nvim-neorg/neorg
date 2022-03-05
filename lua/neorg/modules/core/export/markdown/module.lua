7c8aea7dbd82d4588f403566423eba3a71bd0021
Vhyrro <vhyrro@gmail.com>
2022-03-05 22:12:01 +0100

    fix(export.markdown): improve performance and allow for links with no description

--- lua/neorg/modules/core/export/markdown/module.lua
+++ lua/neorg/modules/core/export/markdown/module.lua
@@ -1,10 +1,15 @@
 local module = neorg.modules.create("core.export.markdown")
 
+-- Multiple `if/elseif` statements exist not to look ugly
+-- but instead to be more perfomant than Neorg's match function
+
+local last_parsed_link_location = ""
+
 module.public = {
     export = {
         functions = {
             ["_word"] = function(text)
-                return text, true
+                return text
             end,
             ["_space"] = function(space)
                 return space
@@ -34,68 +39,78 @@ module.public = {
                 return "###### "
             end,
             ["_open"] = function(_, node)
-                return neorg.lib.match({
-                    node:parent():type(),
-                    bold = "**",
-                    italic = "*",
-                    underline = "__",
-                    strikethrough = "~~",
-                    spoiler = "|",
-                    inline_code = "`",
-                    inline_comment = "<!-- ",
-                })
+                local type = node:parent():type()
+
+                if type == "bold" then
+                    return "**"
+                elseif type == "italic" then
+                    return "_"
+                elseif type == "underline" then
+                    return "__"
+                elseif type == "strikethrough" then
+                    return "~~"
+                elseif type == "spoiler" then
+                    return "|"
+                elseif type == "verbatim" then
+                    return "`"
+                elseif type == "inline_comment" then
+                    return "<!-- "
+                end
             end,
             ["_close"] = function(_, node)
-                return neorg.lib.match({
-                    node:parent():type(),
-                    bold = "**",
-                    italic = "*",
-                    underline = "__",
-                    strikethrough = "~~",
-                    spoiler = "|",
-                    inline_code = "`",
-                    inline_comment = " -->",
-                })
+                local type = node:parent():type()
+
+                if type == "bold" then
+                    return "**"
+                elseif type == "italic" then
+                    return "_"
+                elseif type == "underline" then
+                    return "__"
+                elseif type == "strikethrough" then
+                    return "~~"
+                elseif type == "spoiler" then
+                    return "|"
+                elseif type == "verbatim" then
+                    return "`"
+                elseif type == "inline_comment" then
+                    return " -->"
+                end
             end,
             ["_begin"] = function(text, node)
-                return neorg.lib.match({
-                    node:parent():type(),
+                local type = node:parent():type()
 
-                    link_location = function()
-                        return text == "{" and "("
-                    end,
-                    link_description = function()
-                        return "["
-                    end,
-                })
+                if type == "link_location" then
+                    return text == "{" and "("
+                elseif type == "link_description" then
+                    return "["
+                end
             end,
             ["_end"] = function(text, node)
-                return neorg.lib.match({
-                    node:parent():type(),
+                local type = node:parent():type()
 
-                    link_location = function()
-                        return text == "}" and ")"
-                    end,
-                    link_description = function()
-                        return "]"
-                    end,
-                })
+                if type == "link_location" then
+                    return text == "}" and ")"
+                elseif type == "link_description" then
+                    return "]"
+                end
             end,
             ["link_file_text"] = function(text)
                 return vim.uri_from_fname(text .. ".md"):sub(string.len("file://") + 1)
             end,
         },
+
         recollectors = {
             ["link_location"] = function(output)
                 table.insert(output, #output - 1, "#")
 
+                last_parsed_link_location = output[#output - 1]
                 output[#output - 1] = output[#output - 1]:lower():gsub("[^%s%w]+", ""):gsub("%s+", "-")
 
                 return output
             end,
             ["link"] = function(output)
                 return {
-                    output[2],
+                    output[2] or ("[" .. last_parsed_link_location .. "]"),
                     output[1],
                 }
             end,
