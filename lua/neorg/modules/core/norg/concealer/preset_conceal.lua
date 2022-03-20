cc8442bb944abf1145c0a596b9bd0ce8e1c9dd0d
Vhyrro <vhyrro@gmail.com>
2022-03-20 21:27:39 +0100

    fix: add missing 'conceal' preset

---
+++ lua/neorg/modules/core/norg/concealer/preset_conceal.lua
@@ -0,0 +1,8 @@
+local module = neorg.modules.extend("core.norg.concealer.preset_conceal", "core.norg.concealer")
+
+module.config.private.markup_preset_conceal = {
+    icon = nil,
+    conceal = "",
+}
+
+return module
