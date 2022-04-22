4a56fb363c754bfc0c00afcfaedda74f70fb1dd0
max397574 <81827001+max397574@users.noreply.github.com>
2022-04-22 17:46:39 +0200

    feat: replace beta print in `:Neorg module list` with a UI (#434)

--- lua/neorg/modules/core/neorgcmd/commands/module/list/module.lua
+++ lua/neorg/modules/core/neorgcmd/commands/module/list/module.lua
@@ -12,7 +12,7 @@ require("neorg.modules.base")
 local module = neorg.modules.create("core.neorgcmd.commands.module.list")
 
 module.setup = function()
-    return { success = true, requires = { "core.neorgcmd" } }
+    return { success = true, requires = { "core.neorgcmd", "core.ui" } }
 end
 
 module.public = {
@@ -41,15 +41,35 @@ module.public = {
 
 module.on_event = function(event)
     if event.type == "core.neorgcmd.events.module.list" then
-        vim.cmd('echom "--- PRINTING ALL LOADED MODULES ---"')
+        local lines = {
+            -- neorg.modules.get_module_config("core.norg.concealer").icons.heading.level_1.icon
+            "*" .. " " .. "Loaded Neorg Modules",
+        }
+        local ns = vim.api.nvim_create_namespace("neorg-module-list")
 
         for _, mod in pairs(neorg.modules.loaded_modules) do
-            vim.cmd('echom "' .. mod.name .. '"')
+            table.insert(lines, "  - `" .. mod.name .. "`")
         end
+        local buf = module.required["core.ui"].create_norg_buffer("module_list", "nosplit")
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+        vim.api.nvim_buf_set_keymap(buf, "n", "q", "<cmd>q<CR>", { noremap = true, silent = true, nowait = true })
+        local width = vim.api.nvim_win_get_width(0)
+        local height = vim.api.nvim_win_get_height(0)
 
-        vim.cmd(
-            'echom "Execute :messages to see output. BETA PRINTER FOR LOADED MODULES. This is obviously not final :P. Soon modules will be shown in a floating window."'
-        )
+        vim.api.nvim_open_win(buf, true, {
+            relative = "win",
+            win = 0,
+            width = math.floor(width * 0.7),
+            height = math.floor(height * 0.9),
+            col = math.floor(width * 0.15),
+            row = math.floor(height * 0.05),
+            border = "single",
+            style = "minimal",
+        })
+
+        vim.api.nvim_buf_set_option(buf, "modifiable", false)
+        vim.api.nvim_buf_set_option(buf, "filetype", "norg")
+        vim.api.nvim_buf_set_name(buf, "loaded_modules.norg")
     end
 end
 
