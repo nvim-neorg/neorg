0768c1b3bca16ac541147164f915cc82de81a857
Vhyrro <vhyrro@gmail.com>
2022-06-24 16:37:14 +0200

    ref(injections.scm): make language injections work with new tag node names

--- queries/norg/injections.scm
+++ queries/norg/injections.scm
@@ -1,5 +1,5 @@
 ; Injection for code blocks
-(ranged_verbatim_tag (tag_name) @_tagname (tag_parameters parameter: (tag_param) @language) (ranged_verbatim_tag_content) @content (#eq? @_tagname "code") (#not-eq? @language "norg"))
+(ranged_verbatim_tag (tag_name) @_tagname (tag_parameters (tag_param) @language) (ranged_verbatim_tag_content) @content (#eq? @_tagname "code") (#not-eq? @language "norg"))
 (ranged_verbatim_tag (tag_name) @_tagname (tag_parameters)? (ranged_verbatim_tag_content) @latex (#eq? @_tagname "math"))
 
 ; doesn't work rn: (ranged_tag (tag_name) @_tagname (tag_parameters)? (ranged_tag_content) @norg (#eq? @_tagname "example"))
