9b680e7a8789502f67f195910e06257cf5e52072
Vhyrro <vhyrro@gmail.com>
2022-06-25 14:11:18 +0200

    feat(core.tangle): finalize basic tangling logic

--- lua/neorg/modules/core/tangle/module.lua
+++ lua/neorg/modules/core/tangle/module.lua
@@ -43,14 +43,10 @@ module.public = {
     tangle = function(buffer)
         local parsed_document_metadata = module.required["core.integrations.treesitter"].get_document_metadata(buffer)
 
-        if vim.tbl_isempty(parsed_document_metadata) then
-            log.error("Unable to tangle document - no document metadata present!")
-            return
-        end
-
-        if not parsed_document_metadata.tangle then
-            log.error("Unable to tangle document - no tangling information provided within metadata!")
-            return
+        if vim.tbl_isempty(parsed_document_metadata) or not parsed_document_metadata.tangle then
+            parsed_document_metadata = {
+                tangle = {},
+            }
         end
 
         local document_root = module.required["core.integrations.treesitter"].get_document_root(buffer)
@@ -70,6 +66,8 @@ module.public = {
                     options.languages[language] = file
                 end
             end
+        elseif type(parsed_document_metadata.tangle) == "string" then
+            options.languages[neorg.utils.get_filetype(parsed_document_metadata.tangle)] = parsed_document_metadata.tangle
         end
 
         local tangles = {
@@ -77,31 +75,29 @@ module.public = {
         }
 
         local query_str = neorg.lib.match(options.scope) {
-            all = [[
+            _ = [[
                 (ranged_tag
                     name: (tag_name) @_name
                     (#eq? @_name "code")
                     (tag_parameters
                         .
-                        parameter: (tag_param) @_language)
-                        (#any-of? @_language "%s")) @tag
+                        parameter: (tag_param) @_language)) @tag
             ]],
             tagged = [[
-                (carryover_tag
-                    name: (tag_name) @_carryover_tag_name
-                    (#eq? @_carryover_tag_name "tangle"))
-                .
-                (ranged_tag
-                    name: (tag_name) @_name
-                    (#eq? @_name "code")
-                    (tag_parameters
-                        .
-                        parameter: (tag_param) @_language)
-                        (#any-of? @_language "%s")) @tag
+                (carryover_tag_set
+                    (carryover_tag
+                        name: (tag_name) @_carryover_tag_name
+                        (#eq? @_carryover_tag_name "tangle"))
+                    (ranged_tag
+                        name: (tag_name) @_name
+                        (#eq? @_name "code")
+                        (tag_parameters
+                            .
+                            parameter: (tag_param) @_language)) @tag)
             ]],
         }
 
-        local query = vim.treesitter.parse_query("norg", string.format(query_str, table.concat(vim.tbl_keys(options.languages), "\" \"")))
+        local query = vim.treesitter.parse_query("norg", query_str)
 
         for id, node in query:iter_captures(document_root, buffer, 0, -1) do
             local capture = query.captures[id]
@@ -116,12 +112,18 @@ module.public = {
                         if attribute.name == "tangle.none" then
                             goto skip_tag
                         elseif attribute.name == "tangle" and attribute.parameters[1] then
+                            if options.scope == "main" then
+                                goto skip_tag
+                            end
+
                             file_to_tangle_to = table.concat(attribute.parameters)
                         end
                     end
 
-                    tangles[file_to_tangle_to] = tangles[file_to_tangle_to] or {}
-                    vim.list_extend(tangles[file_to_tangle_to], parsed_tag.content)
+                    if file_to_tangle_to then
+                        tangles[file_to_tangle_to] = tangles[file_to_tangle_to] or {}
+                        vim.list_extend(tangles[file_to_tangle_to], parsed_tag.content)
+                    end
 
                     ::skip_tag::
                 end
@@ -135,7 +137,29 @@ module.public = {
 module.on_event = function(event)
     if event.type == "core.neorgcmd.events.core.tangle.current-file" then
         local tangles = module.public.tangle(event.buffer)
-        log.warn(tangles)
+
+        if not tangles or vim.tbl_isempty(tangles) then
+            vim.notify("Nothing to tangle!")
+            return
+        end
+
+        for file, content in pairs(tangles) do
+            vim.loop.fs_open(file, "w", 438, function(err, fd)
+                assert(
+                    not err,
+                    neorg.lib.lazy_string_concat("Failed to open file '", file, "' for tangling: ", err)
+                )
+
+                vim.loop.fs_write(fd, table.concat(content, "\n"), 0, function(werr)
+                    assert(
+                        not werr,
+                        neorg.lib.lazy_string_concat("Failed to write to file '", file, "' for tangling: ", werr)
+                    )
+                end)
+
+                vim.schedule(neorg.lib.wrap(vim.notify, "Successfully tangled 1 file!"))
+            end)
+        end
     end
 end
 
