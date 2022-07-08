a23d6df77466f356e5cdd107889850a60d3a5c23
Vhyrro <vhyrro@gmail.com>
2022-07-08 17:09:02 +0200

    feat: add `core.looking-glass` to `core.defaults`

--- lua/neorg/modules/core/defaults/module.lua
+++ lua/neorg/modules/core/defaults/module.lua
@@ -10,18 +10,19 @@ modules that any user would want to have a "just works" experience.
 require("neorg.modules.base")
 
 return neorg.modules.create_meta(
-    "core.defaults",
-    "core.syntax",
     "core.autocommands",
-    "core.neorgcmd",
+    "core.defaults",
+    "core.integrations.treesitter",
     "core.keybinds",
+    "core.looking-glass",
     "core.mode",
-    "core.norg.qol.todo_items",
+    "core.neorgcmd",
+    "core.norg.esupports.hop",
     "core.norg.esupports.indent",
     "core.norg.esupports.metagen",
-    "core.norg.esupports.hop",
-    "core.integrations.treesitter",
-    "core.storage",
     "core.norg.news",
+    "core.norg.qol.todo_items",
+    "core.storage",
+    "core.syntax",
     "core.tangle"
 )
