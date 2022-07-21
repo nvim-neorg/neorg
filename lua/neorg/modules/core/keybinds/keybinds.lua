1b640a92e37d7e0a61921ab2db164ac75c70824c
Vhyrro <vhyrro@gmail.com>
2022-07-21 16:34:06 +0200

    docs(core.keybinds): add more documentation to keybinds.lua

--- lua/neorg/modules/core/keybinds/keybinds.lua
+++ lua/neorg/modules/core/keybinds/keybinds.lua
@@ -10,25 +10,61 @@ module.config.public = {
             keybinds.map_event_to_mode("norg", {
                 n = { -- Bind keys in normal mode
 
-                    -- Keys for managing TODO items and setting their states
+                    --> Keys for managing TODO items
+
+                    -- Marks the task under the cursor as "undone"
+                    -- ^mark Task as Undone
                     { "gtu", "core.norg.qol.todo_items.todo.task_undone" },
+
+                    -- Marks the task under the cursor as "pending"
+                    -- ^mark Task as Pending
                     { "gtp", "core.norg.qol.todo_items.todo.task_pending" },
+
+                    -- Marks the task under the cursor as "done"
+                    -- ^mark Task as Done
                     { "gtd", "core.norg.qol.todo_items.todo.task_done" },
+
+                    -- Marks the task under the cursor as "on_hold"
+                    -- ^mark Task as on Hold
                     { "gth", "core.norg.qol.todo_items.todo.task_on_hold" },
+
+                    -- Marks the task under the cursor as "cancelled"
+                    -- ^mark Task as Cancelled
                     { "gtc", "core.norg.qol.todo_items.todo.task_cancelled" },
+                    
+                    -- Marks the task under the cursor as "recurring"
+                    -- ^mark Task as Recurring
                     { "gtr", "core.norg.qol.todo_items.todo.task_recurring" },
+
+                    -- Marks the task under the cursor as "important"
+                    -- ^mark Task as Important
                     { "gti", "core.norg.qol.todo_items.todo.task_important" },
+
+                    -- Switches the task under the cursor between a select few states
                     { "<C-Space>", "core.norg.qol.todo_items.todo.task_cycle" },
 
-                    -- Keys for managing GTD
+                    --> Keys for managing GTD
+
+                    -- Captures a task
+                    -- ^Task Capture
                     { leader .. "tc", "core.gtd.base.capture" },
+
+                    -- Short for "task views", show a view selection menu
+                    -- ^Task View
                     { leader .. "tv", "core.gtd.base.views" },
+
+                    -- Short for "task edit", lets you edit a task
+                    -- ^Task Edit
                     { leader .. "te", "core.gtd.base.edit" },
 
-                    -- Keys for managing notes
+                    -- Creates a new .norg file to take notes in
+                    -- ^New Note
                     { leader .. "nn", "core.norg.dirman.new.note" },
 
+                    -- Hop to the destination of the link under the cursor
                     { "<CR>", "core.norg.esupports.hop.hop-link" },
+
+                    -- Same as `<CR>`, except opens the destination in a vertical split
                     { "<M-CR>", "core.norg.esupports.hop.hop-link", "vsplit" },
                 },
             }, {
@@ -39,8 +75,10 @@ module.config.public = {
             -- Map the below keys only when traverse-heading mode is active
             keybinds.map_event_to_mode("traverse-heading", {
                 n = {
-                    -- Rebind j and k to move between headings in traverse-heading mode
+                    -- Move to the next heading in the document
                     { "j", "core.integrations.treesitter.next.heading" },
+                    
+                    -- Move to the previous heading in the document
                     { "k", "core.integrations.treesitter.previous.heading" },
                 },
             }, {
@@ -50,10 +88,15 @@ module.config.public = {
 
             keybinds.map_event_to_mode("toc-split", {
                 n = {
+                    -- Hop to the target of the TOC link
                     { "<CR>", "core.norg.qol.toc.hop-toc-link" },
 
-                    -- Keys for closing the current display
+                    -- Closes the TOC split
+                    -- ^Quit
                     { "q", "core.norg.qol.toc.close" },
+
+                    -- Closes the TOC split
+                    -- ^Escape
                     { "<Esc>", "core.norg.qol.toc.close" },
                 },
             }, {
