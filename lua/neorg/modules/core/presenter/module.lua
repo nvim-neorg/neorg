ad5284450bbf22910f0febcfb062ad2bfc23e43f
Vhyrro <vhyrro@gmail.com>
2022-08-19 22:03:34 +0200

    docs(core.presenter): try to fix dead links take 2

--- lua/neorg/modules/core/presenter/module.lua
+++ lua/neorg/modules/core/presenter/module.lua
@@ -66,8 +66,8 @@ end
 
 module.config.public = {
     -- Zen mode plugin to use. Currenly suppported:
-    -- `zen-mode` - https://github.com/folke/zen-mode.nvim
-    -- `truezen`  - https://github.com/Pocco81/TrueZen.nvim
+    -- - `zen-mode` (<https://github.com/folke/zen-mode.nvim>)
+    -- - `truezen` (<https://github.com/Pocco81/TrueZen.nvim>)
     zen_mode = "",
 }
 
