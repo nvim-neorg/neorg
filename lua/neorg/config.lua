f2d5ecdd2ef472152f5ccd352bbc324109174a2f
Vhyrro <vhyrro@gmail.com>
2022-08-26 16:24:27 +0200

    feat: add `neorg.configuration.neovim_version` key

--- lua/neorg/config.lua
+++ lua/neorg/config.lua
@@ -14,6 +14,46 @@ neorg.configuration = {
     arguments = {},
 
     version = "0.0.12",
+    neovim_version = (function()
+        require("neorg.external.helpers")
+
+        local data = {}
+        local parsed_output = vim.api.nvim_exec("version", true)
+
+        for _, line in ipairs(vim.split(parsed_output, "\n")) do
+            local key, value = line:match("^%s*(.+[^%s]):%s+(.+)$")
+
+            if not key then
+                key, value = line:match("^(NVIM)%s+v%d+%.%d+%.%d+%-%w+%-(%d+).+")
+            end
+
+            if not key then
+                key, value = line:match("(LUAJIT)%s+(.+)")
+            end
+
+            if key then
+                key = key:lower():gsub("%p", ""):gsub("%s", "-")
+
+                value = neorg.lib.match(key)({
+                    compilation = function()
+                        local split = vim.split(value, "%s+")
+
+                        split.compiler = table.remove(split, 1)
+                        return split
+                    end,
+                    features = neorg.lib.wrap(vim.split, value, "%s*%+", {
+                        trimempty = true,
+                    }),
+                    nvim = tonumber(value),
+                    _ = value:gsub('^"?', ""):gsub('"?$', ""),
+                })
+
+                data[key] = value
+            end
+        end
+
+        return data
+    end)(),
 }
 
 -- Grab OS info on startup
