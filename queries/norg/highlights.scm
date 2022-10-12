1f222a89dbca9ace3788113595cc024cd2af590e
Vhyrro <vhyrro@gmail.com>
2022-10-12 17:31:32 +0200

    feat(highlights): add support for headings, errors, escape sequences and definitions

--- queries/norg/highlights.scm
+++ queries/norg/highlights.scm
@@ -120,87 +120,58 @@
        ((link_target_timestamp) @neorg.links.location.timestamp.prefix
           (paragraph) @neorg.links.location.timestamp)]
       ("_end") @neorg.links.location.delimiter))
-; 
-; ; Headings
-; (heading1 (heading1_prefix) @neorg.headings.1.prefix title: (paragraph_segment) @neorg.headings.1.title)
-; (heading2 (heading2_prefix) @neorg.headings.2.prefix title: (paragraph_segment) @neorg.headings.2.title)
-; (heading3 (heading3_prefix) @neorg.headings.3.prefix title: (paragraph_segment) @neorg.headings.3.title)
-; (heading4 (heading4_prefix) @neorg.headings.4.prefix title: (paragraph_segment) @neorg.headings.4.title)
-; (heading5 (heading5_prefix) @neorg.headings.5.prefix title: (paragraph_segment) @neorg.headings.5.title)
-; (heading6 (heading6_prefix) @neorg.headings.6.prefix title: (paragraph_segment) @neorg.headings.6.title)
-; 
-; ; Display errors
-; (ERROR) @neorg.error
-; 
-; ; Markers
-; (marker (marker_prefix) @neorg.markers.prefix (paragraph_segment) @neorg.markers.title)
-; 
-; ; Definitions
-; (single_definition (single_definition_prefix) @neorg.definitions.prefix title: (paragraph_segment) @neorg.definitions.title content: [(_) "_paragraph_break"]* @neorg.definitions.content)
-; (multi_definition (multi_definition_prefix) @neorg.definitions.prefix title: (paragraph_segment) @neorg.definitions.title content: [(_) "_paragraph_break"]* @neorg.definitions.content end: (multi_definition_suffix) @neorg.definitions.suffix)
-; 
-; ; Footnotes
-; (single_footnote (single_footnote_prefix) @neorg.footnotes.prefix title: (paragraph_segment) @neorg.footnotes.title content: [(_) "_paragraph_break"]* @neorg.footnotes.content)
-; (multi_footnote (multi_footnote_prefix) @neorg.footnotes.prefix title: (paragraph_segment) @neorg.footnotes.title content: [(_) "_paragraph_break"]* @neorg.footnotes.content end: (multi_footnote_suffix) @neorg.footnotes.suffix)
-; 
-; ; Escape sequences (\char)
-; (escape_sequence) @neorg.modifiers.escape
-; 
-; ; Todo Items
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_undone)    @neorg.todo_items.undone.1    content: (paragraph) @neorg.todo_items.undone.1.content)
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_pending)   @neorg.todo_items.pending.1   content: (paragraph) @neorg.todo_items.pending.1.content)
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_done)      @neorg.todo_items.done.1      content: (paragraph) @neorg.todo_items.done.1.content)
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_on_hold)   @neorg.todo_items.on_hold.1   content: (paragraph) @neorg.todo_items.on_hold.1.content)
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_cancelled) @neorg.todo_items.cancelled.1 content: (paragraph) @neorg.todo_items.cancelled.1.content)
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_urgent)    @neorg.todo_items.urgent.1    content: (paragraph) @neorg.todo_items.urgent.1.content)
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_uncertain) @neorg.todo_items.uncertain.1 content: (paragraph) @neorg.todo_items.uncertain.1.content)
-; (todo_item1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix state: (todo_item_recurring) @neorg.todo_items.recurring.1 content: (paragraph) @neorg.todo_items.recurring.1.content)
-; 
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_undone)    @neorg.todo_items.undone.2    content: (paragraph) @neorg.todo_items.undone.2.content)
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_pending)   @neorg.todo_items.pending.2   content: (paragraph) @neorg.todo_items.pending.2.content)
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_done)      @neorg.todo_items.done.2      content: (paragraph) @neorg.todo_items.done.2.content)
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_on_hold)   @neorg.todo_items.on_hold.2   content: (paragraph) @neorg.todo_items.on_hold.2.content)
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_cancelled) @neorg.todo_items.cancelled.2 content: (paragraph) @neorg.todo_items.cancelled.2.content)
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_urgent)    @neorg.todo_items.urgent.2    content: (paragraph) @neorg.todo_items.urgent.2.content)
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_uncertain) @neorg.todo_items.uncertain.2 content: (paragraph) @neorg.todo_items.uncertain.2.content)
-; (todo_item2 (unordered_list2_prefix) @neorg.lists.unordered.2.prefix state: (todo_item_recurring) @neorg.todo_items.recurring.2 content: (paragraph) @neorg.todo_items.recurring.2.content)
-; 
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_undone)    @neorg.todo_items.undone.3    content: (paragraph) @neorg.todo_items.undone.3.content)
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_pending)   @neorg.todo_items.pending.3   content: (paragraph) @neorg.todo_items.pending.3.content)
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_done)      @neorg.todo_items.done.3      content: (paragraph) @neorg.todo_items.done.3.content)
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_on_hold)   @neorg.todo_items.on_hold.3   content: (paragraph) @neorg.todo_items.on_hold.3.content)
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_cancelled) @neorg.todo_items.cancelled.3 content: (paragraph) @neorg.todo_items.cancelled.3.content)
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_urgent)    @neorg.todo_items.urgent.3    content: (paragraph) @neorg.todo_items.urgent.3.content)
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_uncertain) @neorg.todo_items.uncertain.3 content: (paragraph) @neorg.todo_items.uncertain.3.content)
-; (todo_item3 (unordered_list3_prefix) @neorg.lists.unordered.3.prefix state: (todo_item_recurring) @neorg.todo_items.recurring.3 content: (paragraph) @neorg.todo_items.recurring.3.content)
-; 
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_undone)    @neorg.todo_items.undone.4    content: (paragraph) @neorg.todo_items.undone.4.content)
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_pending)   @neorg.todo_items.pending.4   content: (paragraph) @neorg.todo_items.pending.4.content)
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_done)      @neorg.todo_items.done.4      content: (paragraph) @neorg.todo_items.done.4.content)
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_on_hold)   @neorg.todo_items.on_hold.4   content: (paragraph) @neorg.todo_items.on_hold.4.content)
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_cancelled) @neorg.todo_items.cancelled.4 content: (paragraph) @neorg.todo_items.cancelled.4.content)
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_urgent)    @neorg.todo_items.urgent.4    content: (paragraph) @neorg.todo_items.urgent.4.content)
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_uncertain) @neorg.todo_items.uncertain.4 content: (paragraph) @neorg.todo_items.uncertain.4.content)
-; (todo_item4 (unordered_list4_prefix) @neorg.lists.unordered.4.prefix state: (todo_item_recurring) @neorg.todo_items.recurring.4 content: (paragraph) @neorg.todo_items.recurring.4.content)
-; 
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_undone)    @neorg.todo_items.undone.5    content: (paragraph) @neorg.todo_items.undone.5.content)
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_pending)   @neorg.todo_items.pending.5   content: (paragraph) @neorg.todo_items.pending.5.content)
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_done)      @neorg.todo_items.done.5      content: (paragraph) @neorg.todo_items.done.5.content)
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_on_hold)   @neorg.todo_items.on_hold.5   content: (paragraph) @neorg.todo_items.on_hold.5.content)
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_cancelled) @neorg.todo_items.cancelled.5 content: (paragraph) @neorg.todo_items.cancelled.5.content)
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_urgent)    @neorg.todo_items.urgent.5    content: (paragraph) @neorg.todo_items.urgent.5.content)
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_uncertain) @neorg.todo_items.uncertain.5 content: (paragraph) @neorg.todo_items.uncertain.5.content)
-; (todo_item5 (unordered_list5_prefix) @neorg.lists.unordered.5.prefix state: (todo_item_recurring) @neorg.todo_items.recurring.5 content: (paragraph) @neorg.todo_items.recurring.5.content)
-; 
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_undone)    @neorg.todo_items.undone.6    content: (paragraph) @neorg.todo_items.undone.6.content)
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_pending)   @neorg.todo_items.pending.6   content: (paragraph) @neorg.todo_items.pending.6.content)
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_done)      @neorg.todo_items.done.6      content: (paragraph) @neorg.todo_items.done.6.content)
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_on_hold)   @neorg.todo_items.on_hold.6   content: (paragraph) @neorg.todo_items.on_hold.6.content)
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_cancelled) @neorg.todo_items.cancelled.6 content: (paragraph) @neorg.todo_items.cancelled.6.content)
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_urgent)    @neorg.todo_items.urgent.6    content: (paragraph) @neorg.todo_items.urgent.6.content)
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_uncertain) @neorg.todo_items.uncertain.6 content: (paragraph) @neorg.todo_items.uncertain.6.content)
-; (todo_item6 (unordered_list6_prefix) @neorg.lists.unordered.6.prefix state: (todo_item_recurring) @neorg.todo_items.recurring.6 content: (paragraph) @neorg.todo_items.recurring.6.content)
-; 
+
+; Headings
+(heading1
+  (heading1_prefix) @neorg.headings.1.prefix
+  title: (paragraph_segment) @neorg.headings.1.title)
+(heading2
+  (heading2_prefix) @neorg.headings.2.prefix
+  title: (paragraph_segment) @neorg.headings.2.title)
+(heading3
+  (heading3_prefix) @neorg.headings.3.prefix
+  title: (paragraph_segment) @neorg.headings.3.title)
+(heading4
+  (heading4_prefix) @neorg.headings.4.prefix
+  title: (paragraph_segment) @neorg.headings.4.title)
+(heading5
+  (heading5_prefix) @neorg.headings.5.prefix
+  title: (paragraph_segment) @neorg.headings.5.title)
+(heading6
+  (heading6_prefix) @neorg.headings.6.prefix
+  title: (paragraph_segment) @neorg.headings.6.title)
+
+; Display errors
+(ERROR) @neorg.error
+
+; Definitions
+(single_definition
+  (single_definition_prefix) @neorg.definitions.prefix
+  title: (paragraph_segment) @neorg.definitions.title
+  content: [(_) "_paragraph_break"]* @neorg.definitions.content)
+(multi_definition
+  (multi_definition_prefix) @neorg.definitions.prefix
+  title: (paragraph_segment) @neorg.definitions.title
+  content: [(_) "_paragraph_break"]* @neorg.definitions.content
+  end: (multi_definition_suffix) @neorg.definitions.suffix)
+
+; Footnotes
+(single_footnote
+  (single_footnote_prefix) @neorg.footnotes.prefix
+  title: (paragraph_segment) @neorg.footnotes.title
+  content: [(_) "_paragraph_break"]* @neorg.footnotes.content)
+(multi_footnote
+  (multi_footnote_prefix) @neorg.footnotes.prefix
+  title: (paragraph_segment) @neorg.footnotes.title
+  content: [(_) "_paragraph_break"]* @neorg.footnotes.content
+  end: (multi_footnote_suffix) @neorg.footnotes.suffix)
+
+; Escape sequences (\char)
+(escape_sequence) @neorg.modifiers.escape
+
+; Detached Modifier extensions
+; TODO
+
 ; 
 ; ; Unordered lists
 ; (unordered_list1 (unordered_list1_prefix) @neorg.lists.unordered.1.prefix content: (paragraph) @neorg.lists.unordered.1.content)
@@ -217,22 +188,6 @@
 ; (ordered_list4 (ordered_list4_prefix) @neorg.lists.ordered.4.prefix content: (paragraph) @neorg.lists.ordered.4.content)
 ; (ordered_list5 (ordered_list5_prefix) @neorg.lists.ordered.5.prefix content: (paragraph) @neorg.lists.ordered.5.content)
 ; (ordered_list6 (ordered_list6_prefix) @neorg.lists.ordered.6.prefix content: (paragraph) @neorg.lists.ordered.6.content)
-; 
-; ; Unordered links (DEPRECATED)
-; ; (unordered_link1 (unordered_link1_prefix) location: (link) )
-; ; (unordered_link2 (unordered_link2_prefix) location: (link) )
-; ; (unordered_link3 (unordered_link3_prefix) location: (link) )
-; ; (unordered_link4 (unordered_link4_prefix) location: (link) )
-; ; (unordered_link5 (unordered_link5_prefix) location: (link) )
-; ; (unordered_link6 (unordered_link6_prefix) location: (link) )
-; 
-; ; Ordered links (DEPRECATED)
-; ; (ordered_link1 (ordered_link1_prefix) location: (link) )
-; ; (ordered_link2 (ordered_link2_prefix) location: (link) )
-; ; (ordered_link3 (ordered_link3_prefix) location: (link) )
-; ; (ordered_link4 (ordered_link4_prefix) location: (link) )
-; ; (ordered_link5 (ordered_link5_prefix) location: (link) )
-; ; (ordered_link6 (ordered_link6_prefix) location: (link) )
 ; 
 ; ; Quotes
 ; (quote1 (quote1_prefix) @neorg.quotes.1.prefix content: (paragraph_segment) @neorg.quotes.1.content)
