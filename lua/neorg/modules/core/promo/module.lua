038b714d25776514b8d993586b1c0b6c3e51f2c3
Vhyrro <vhyrro@gmail.com>
2022-10-22 18:00:12 +0200

    ref(promo): flatten out the `promote_or_demote` function

--- lua/neorg/modules/core/promo/module.lua
+++ lua/neorg/modules/core/promo/module.lua
@@ -49,83 +49,68 @@ module.private = {
 }
 
 module.public = {
-    find_node = function(node)
-        while node do
-            for _, type in pairs(module.private.types) do
-                if node:type():match(type.pattern) then
-                    local level = tonumber(node:type():match(type.pattern))
-                    return node, type.prefix, level
-                end
+    get_promotable_node_prefix = function(node)
+        for _, data in pairs(module.private.types) do
+            if node:type():match(data.pattern) then
+                return data.prefix
             end
-            node = node:parent()
         end
     end,
 
-    promote_or_demote = function(event, mode, row)
-        local cursor = not row
-        local start_row
-        local cursor_pos = vim.api.nvim_win_get_cursor(event.window)
-        row = row or cursor_pos[1]
-        local start_node = module.required["core.integrations.treesitter"].get_first_node_on_line(event.buffer, row)
-        local node, prefix, level = module.public.find_node(start_node)
-        if node then
-            start_row, _, _, _ = node:range()
-            -- cursor is on heading title
-            if row == start_row + 1 then
-                local title = vim.treesitter.get_node_text(node, event.buffer, { concat = false })
-                -- remove prefix
-                local title_text = (title[1]):sub(level + 2)
-                local new_level
-                if mode == "promote" then
-                    -- TODO: do we want it like this?
-                    new_level = level < 6 and level + 1 or 6
-                elseif mode == "demote" then
-                    new_level = level > 1 and level - 1 or 1
-                end
-                vim.api.nvim_buf_set_lines(
-                    event.buffer,
-                    start_row,
-                    start_row + 1,
-                    false,
-                    { string.rep(prefix, new_level) .. " " .. title_text }
-                )
-            else
-                if cursor then
-                    local lines = vim.api.nvim_buf_get_lines(event.buffer, row - 1, row, false)
-                    if mode == "promote" then
-                        lines[1] = string.rep(" ", vim.bo[event.buffer].shiftwidth) .. lines[1]
-                        vim.api.nvim_buf_set_lines(event.buffer, row - 1, row, false, lines)
-                    elseif mode == "demote" then
-                        local spaces = #lines[1]:match("^(%s*)")
-                        lines[1] = string.rep(
-                            " ",
-                            spaces > vim.bo[event.buffer].shiftwidth and spaces - vim.bo[event.buffer].shiftwidth or 0
-                        ) .. lines[1]:gsub("^%s*", "")
-                        vim.api.nvim_buf_set_lines(event.buffer, row - 1, row, false, lines)
-                    end
-                end
-            end
+    promote_or_demote = function(buffer, mode, row)
+        local node = module.required["core.integrations.treesitter"].get_first_node_on_line(buffer, row)
+
+        local prefix = module.public.get_promotable_node_prefix(node)
+
+        if not prefix then
+            vim.api.nvim_feedkeys(mode == "promote" and ">>" or "<<", "n", false)
+            return
         end
-        local concealer_event = neorg.events.create(
-            module,
-            "core.norg.concealer.events.update_region",
-            { start = start_row, ["end"] = start_row + 2 }
+
+        local title_node = node:named_child(0)
+        local title_range = module.required["core.integrations.treesitter"].get_node_range(title_node)
+        local title = module.required["core.integrations.treesitter"].get_node_text(title_node, buffer)
+
+        do
+            if mode == "promote" then
+                title = table.concat({ prefix, title })
+            elseif mode == "demote" then
+                if title:match(table.concat({ "^", prefix, "*" })):len() <= 1 then
+                    return
+                end
+
+                title = title:sub(2)
+            end
+
+            vim.api.nvim_buf_set_text(
+                buffer,
+                title_range.row_start,
+                title_range.column_start,
+                title_range.row_end,
+                title_range.column_end,
+                { title }
+            )
+        end
+
+        neorg.events.broadcast_event(
+            neorg.events.create(module, "core.norg.concealer.events.update_region", { start = row, ["end"] = row + 1 })
         )
-        neorg.events.broadcast_event(concealer_event, function() end)
     end,
 }
 
 module.on_event = function(event)
+    local row = event.cursor_position[1] - 1
+
     if event.split_type[1] == "core.keybinds" then
         if event.split_type[2] == "core.promo.promote" then
-            module.public.promote_or_demote(event, "promote")
+            module.public.promote_or_demote(event.buffer, "promote", row)
         elseif event.split_type[2] == "core.promo.demote" then
-            module.public.promote_or_demote(event, "demote")
+            module.public.promote_or_demote(event.buffer, "demote", row)
         elseif event.split_type[2] == "core.promo.promote_range" then
             local start_pos = vim.api.nvim_buf_get_mark(event.buffer, "<")
             local end_pos = vim.api.nvim_buf_get_mark(event.buffer, ">")
             for i = 0, end_pos[1] - start_pos[1] do
-                module.public.promote_or_demote(event, "promote", start_pos[1] + i)
+                module.public.promote_or_demote(event.buffer, "promote", start_pos[1] + i)
             end
             local concealer_event = neorg.events.create(
                 module,
@@ -137,7 +122,7 @@ module.on_event = function(event)
             local start_pos = vim.api.nvim_buf_get_mark(event.buffer, "<")
             local end_pos = vim.api.nvim_buf_get_mark(event.buffer, ">")
             for i = 0, end_pos[1] - start_pos[1] do
-                module.public.promote_or_demote(event, "demote", start_pos[1] + i)
+                module.public.promote_or_demote(event.buffer, "demote", start_pos[1] + i)
             end
             local concealer_event = neorg.events.create(
                 module,
