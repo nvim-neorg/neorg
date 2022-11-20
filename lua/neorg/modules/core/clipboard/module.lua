74e862294bbc08d9a37467dccb6e2fd1b5bbf991
Vhyrro <vhyrro@gmail.com>
2022-11-20 19:45:24 +0100

    feat(core.clipboard): finalize clipboard module

--- lua/neorg/modules/core/clipboard/module.lua
+++ lua/neorg/modules/core/clipboard/module.lua
@@ -11,24 +11,45 @@ end
 module.load = function()
     vim.api.nvim_create_autocmd("TextYankPost", {
         callback = function(data)
-            if vim.api.nvim_buf_get_option(data.buf, "filetype") ~= "norg" then
+            if vim.api.nvim_buf_get_option(data.buf, "filetype") ~= "norg" or vim.v.event.operator ~= "y" then
                 return
             end
 
             local range = { vim.api.nvim_buf_get_mark(data.buf, "["), vim.api.nvim_buf_get_mark(data.buf, "]") }
+            range[1][1] = range[1][1] - 1
+            range[2][1] = range[2][1] - 1
 
             for i = range[1][1], range[2][1] do
                 local node = module.required["core.integrations.treesitter"].get_first_node_on_line(data.buf, i)
 
                 while node:parent() do
                     if module.private.callbacks[node:type()] then
-                        local register = vim.fn.getreg(vim.v.event.regname)
+                        local register = vim.fn.getreg(vim.v.register)
 
                         vim.fn.setreg(
-                            vim.v.event.regname,
-                            neorg.lib.filter(module.private.callbacks[node:type()], function(_, cb)
-                                return cb(node, register, { i, range[2] })
-                            end)
+                            vim.v.register,
+                            neorg.lib.filter(module.private.callbacks[node:type()], function(_, callback)
+                                if callback.strict and (range[1][1] < i or range[2][1] > node:end_()) then
+                                    return
+                                end
+
+                                return callback.cb(
+                                    node,
+                                    vim.split(register, "\n", {
+                                        plain = true,
+                                        -- TODO: This causes problems in places
+                                        -- where you actually want to copy
+                                        -- newlines.
+                                        trimempty = true,
+                                    }),
+                                    {
+                                        start = range[1],
+                                        ["end"] = range[2],
+                                        current = { i, range[1][2] },
+                                    }
+                                )
+                            end) or register,
+                            "l"
                         )
 
                         return
@@ -46,9 +67,9 @@ module.private = {
 }
 
 module.public = {
-    add_callback = function(node_type, func)
+    add_callback = function(node_type, func, strict)
         module.private.callbacks[node_type] = module.private.callbacks[node_type] or {}
-        table.insert(module.private.callbacks[node_type], func)
+        table.insert(module.private.callbacks[node_type], { cb = func, strict = strict })
     end,
 }
 
