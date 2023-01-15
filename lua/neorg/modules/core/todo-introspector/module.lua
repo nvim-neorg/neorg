58ccb821b077f59211bcf2fd0be6f1d8594efef5
Vhyrro <vhyrro@gmail.com>
2023-01-15 12:59:45 +0100

    feat(todo-introspector): add proper aggregate functions for two node types

--- lua/neorg/modules/core/todo-introspector/module.lua
+++ lua/neorg/modules/core/todo-introspector/module.lua
@@ -16,80 +16,131 @@ module.load = function()
     vim.api.nvim_create_autocmd("BufEnter", {
         pattern = "*.norg",
         callback = function()
-            for _, provider in ipairs(module.config.public.providers) do
+            for _, provider in pairs(module.config.public.providers) do
                 local capture_map = {
                     -- capture_name = node,
                 }
 
                 module.required["core.integrations.treesitter"].execute_query(provider.query, function(query, id, node)
                     local capture_name = query.captures[id]
-                    capture_map[capture_name] = node
-                end)
 
-                provider.callback(capture_map)
+                    capture_map[capture_name] = node
+
+                    if capture_name == "target" then
+                        local aggregates = provider.aggregate(capture_map)
+
+                        if aggregates and provider.display then
+                            provider.display(aggregates)
+                        end
+
+                        capture_map = {}
+                    end
+                end)
             end
         end,
     })
 end
 
-module.config.public = {
-    providers = {
-        ["heading%d"] = {
-            query = [[
-            ]],
-            -- TODO: What should <type> be?
-            -- Should the order be:
-            -- - Headings aggregate lists
-            -- - Lists aggregate sublists
-            -- Where they only display their completion if they have aggregated at least one object?
-            -- This may complicate things but will make for a better user exp
-            callback = module.public.display_todo_completion("(<undone> of <done> <type>) [<percentage>% complete]"),
-        },
-        ["generic_list"] = {
-            query = [[]],
-            callback = module.public.display_todo_completion("(<undone>/<done>) [<percentage>%]"),
-        },
-    },
+module.public = {
+    aggregate_recursively = function(callback)
+        return function(capture_map)
+            local parent_node = capture_map.target
 
-    anchor_points = {
-        "^generic_list$",
-    },
+            if not parent_node then
+                return
+            end
 
-    todo_types = {
-        "^unordered_list%d$",
-        "^ordered_list%d$",
+            local aggregates = {}
+
+            aggregates.self = neorg.lib.eval(callback, parent_node) or {}
+
+            for child in parent_node:iter_children() do
+                for pattern, data in pairs(module.config.public.providers) do
+                    if child:type():match(table.concat({ "^", pattern, "$" })) then
+                        aggregates[child:type()] = aggregates[child:type()] or {}
+
+                        table.insert(
+                            aggregates[child:type()],
+                            vim.tbl_deep_extend("force", data.aggregate({
+                                target = child,
+                            }) or {}, neorg.lib.eval(callback, child) or {})
+                        )
+                    end
+                end
+            end
+
+            return not vim.tbl_isempty(aggregates) and { [parent_node:type()] = aggregates } or nil
+        end
+    end,
+
+    aggregate_functions = {
+        generic_list = function(node)
+            local info = {
+                -- undone = 0,
+                -- done = 0,
+                -- pending = 0,
+                -- urgent = 0,
+                -- cancelled = 0,
+                -- on_hold = 0,
+                -- recurring = 0,
+            }
+
+            for child in node:iter_children() do
+                local detached_modifier_extension = child:field("state")[1]
+
+                if not detached_modifier_extension then
+                    goto continue
+                end
+
+                local todo_node = nil
+
+                do
+                    for task in detached_modifier_extension:iter_children() do
+                        if vim.startswith(task:type(), "todo_item_") then
+                            todo_node = task
+                            break
+                        end
+                    end
+                end
+
+                if not todo_node then
+                    goto continue
+                end
+
+                local todo_item_type = todo_node:type():match("^todo_item_(.+)$")
+                info[todo_item_type] = (info[todo_item_type] or 0) + 1
+
+                ::continue::
+            end
+
+            return {
+                task_info = info,
+            }
+        end,
     },
 }
 
-module.public = {
-    display_todo_completion = function(format)
-        return function(capture_map)
-        end
-    end,
-
-    analyze_todo_completion = function(node, types, callback)
-        for child in node:iter_children() do
-            local matching_child = neorg.lib.filter(types, function(_, type)
-                return child:type():match(type) and child or nil
-            end)
-
-            if not matching_child then
-                goto continue
-            end
-
-            local detached_modifer_extension = matching_child:field("state")[1]
-
-            if not detached_modifer_extension then
-                goto continue
-            end
-
-            for extension in detached_modifer_extension:iter_children() do
-                callback(extension)
-            end
-
-            ::continue::
-        end
-    end,
+module.config.public = {
+    providers = {
+        ["heading%d"] = {
+            query = [=[
+            [
+                (heading1)
+                (heading2)
+                (heading3)
+                (heading4)
+                (heading5)
+                (heading6)
+            ] @target
+            ]=],
+            aggregate = module.public.aggregate_recursively(),
+            display = module.public.display_heading_completion(),
+        },
+        ["generic_list"] = {
+            query = [[(generic_list) @target]],
+            aggregate = module.public.aggregate_recursively(module.public.aggregate_functions.generic_list),
+        },
+    },
 }
 
 return module
