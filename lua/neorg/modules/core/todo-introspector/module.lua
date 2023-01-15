34507f68c2b977b22ede518d2119a5879c6091fd
Vhyrro <vhyrro@gmail.com>
2023-01-15 14:04:23 +0100

    feat(todo-introspector): add `low` detail completion display

--- lua/neorg/modules/core/todo-introspector/module.lua
+++ lua/neorg/modules/core/todo-introspector/module.lua
@@ -16,15 +16,16 @@ module.load = function()
     vim.api.nvim_create_autocmd("BufEnter", {
         pattern = "*.norg",
         callback = function()
-            for _, provider in pairs(module.config.public.providers) do
+            for match, provider in pairs(module.config.public.providers) do
                 module.required["core.integrations.treesitter"].execute_query(provider.query, function(query, id, node)
                     local capture_name = query.captures[id]
 
                     if capture_name == "target" then
                         local aggregates = provider.aggregate({ target = node })
+                        aggregates.range = module.required["core.integrations.treesitter"].get_node_range(node)
 
                         if aggregates and provider.display then
-                            provider.display(aggregates)
+                            provider.display(aggregates, module.config.public.detail[match])
                         end
                     end
                 end)
@@ -49,7 +50,10 @@ module.public = {
             for child in parent_node:iter_children() do
                 for pattern, data in pairs(module.config.public.providers) do
                     if child:type():match(table.concat({ "^", pattern, "$" })) then
-                        aggregates[child:type()] = aggregates[child:type()] or {}
+                        aggregates[child:type()] = aggregates[child:type()]
+                            or {
+                                range = module.required["core.integrations.treesitter"].get_node_range(child),
+                            }
 
                         table.insert(
                             aggregates[child:type()],
@@ -92,11 +96,59 @@ module.public = {
         end,
     },
 
-    display_heading_completion = function(aggregates)
-        do
-            log.warn(aggregates)
+    display_heading_completion = function(aggregates, detail)
+        if not detail or detail == "high" then
+            module.public.heading_completion.complex_heading_completion(aggregates)
+        else
+            module.public.heading_completion.basic_heading_completion(aggregates)
         end
     end,
+
+    heading_completion = {
+        basic_heading_completion = function(aggregates)
+            if not aggregates.generic_list then
+                return
+            end
+
+            local counts = {
+                undone = 0,
+                done = 0,
+                pending = 0,
+                on_hold = 0,
+                cancelled = 0,
+                urgent = 0,
+                recurring = 0,
+            }
+
+            for _, list in ipairs(aggregates.generic_list) do
+                for _, items in pairs(list) do
+                    for _, item in ipairs(items) do
+                        if item.self[1] then
+                            counts[item.self[1]] = (counts[item.self[1]] or 0) + 1
+                        end
+                    end
+                end
+            end
+
+            local all = counts.undone + counts.pending + counts.done
+            local percentage = math.floor((counts.done / all) * 100)
+
+            vim.api.nvim_buf_set_extmark(0, module.private.namespace, aggregates.range.row_start, 0, {
+                virt_text = {
+                    { "(", "NonText" },
+                    { tostring(counts.done), "@number" },
+                    { " of ", "NonText" },
+                    { tostring(all), (counts.urgent > 1 and "@text.danger" or "@number") },
+                    { ") [", "NonText" },
+                    { tostring(percentage), "@number" },
+                    { "%", "@operator" },
+                    { " complete]", "NonText" },
+                },
+            })
+        end,
+
+        complex_heading_completion = function(aggregates) end,
+    },
 }
 
 module.config.public = {
@@ -141,10 +193,14 @@ module.config.public = {
     },
 
     detail = {
-        ["heading%d"] = "high",
+        ["heading%d"] = "low",
         -- TODO: Implement high detail level for unordered lists
         ["unordered_list%d"] = "low",
     },
+}
+
+module.private = {
+    namespace = vim.api.nvim_create_namespace("neorg/todo-introspector"),
 }
 
 return module
