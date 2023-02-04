8051ad5487a9248f9e6bf6ae9def55c8a58cc6c5
Vhyrro <vhyrro@gmail.com>
2023-02-04 16:26:58 +0100

    docs(integrations.treesitter): add embed, reword title

--- lua/neorg/modules/core/integrations/treesitter/module.lua
+++ lua/neorg/modules/core/integrations/treesitter/module.lua
@@ -1,7 +1,8 @@
 --[[
     file: Treesitter-Integration
-    title: Treesitter integration in Neorg
+    title: Snazzy Treesitter Integration
     summary: A module designed to integrate Treesitter into Neorg.
+    embed: https://user-images.githubusercontent.com/76052559/151668244-9805afc4-8c50-4925-85ec-1098aff5ede6.gif
     ---
 --]]
 
