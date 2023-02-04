3c0d1a136ef5a354ec1eb9ee3ae0619dfcac4029
Vhyrro <vhyrro@gmail.com>
2023-02-04 22:04:47 +0100

    docs(core.promo): add documentation

--- lua/neorg/modules/core/promo/module.lua
+++ lua/neorg/modules/core/promo/module.lua
@@ -1,6 +1,27 @@
 --[[
     file: Promo
+    title: You have Received a Promotion!
+    description: The `promo` module increases or decreases the nesting level of nestable items by repeating their characters.
     summary: Promotes or demotes nestable items within Neorg files.
+    ---
+When dealing with Norg, it may sometimes be tedious to continually repeat a single character to increase
+your nesting level. For example, for a level 6 nested unordered list, you need to repeat the `-` character
+six times:
+```norg
+------ This is my item!
+```
+
+The `core.promo` module allows you to indent these object by utilizing the inbuilt Neovim keybinds:
+- `>>` - increase the indentation level for the current object (also dedents children)
+- `<<` - decrease the indentation level for the current object recursively (also dedents children)
+- `>.` - increase the indentation level for the current object (non-recursively)
+- `<,` - decrease the indentation level for the current object (non-recursively)
+
+In insert mode, you are also provided with two keybinds, also being Neovim defaults:
+- `<C-t>` increase the indentation level for the current object
+- `<C-d>` decrease the indentation level for the current object
+
+This module is commonly used with the [`core.itero`](@core.itero) module for an effective workflow.
 --]]
 
 local module = neorg.modules.create("core.promo")
@@ -30,8 +51,6 @@ module.load = function()
     )
 end
 
-module.config.public = {}
-
 module.private = {
     types = {
         heading = {
@@ -49,10 +68,6 @@ module.private = {
         quote = {
             pattern = "^quote(%d)$",
             prefix = ">",
-        },
-        todo_item = {
-            pattern = "^todo_item(%d)$",
-            prefix = "-",
         },
     },
 
