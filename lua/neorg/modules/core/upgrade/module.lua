e068901664e1dd167cbaee721b284332c1b16f0f
Vhyrro <vhyrro@gmail.com>
2023-02-05 17:32:50 +0100

    docs(core.upgrade): add documentation

--- lua/neorg/modules/core/upgrade/module.lua
+++ lua/neorg/modules/core/upgrade/module.lua
@@ -1,7 +1,21 @@
 --[[
     file: Upgrade
-    title: Upgrade tool for Neorg
+    title: Upgrade Tool for Neorg
     summary: Converts old versions of the Norg syntax to newer ones.
+    ---
+When dealing with changes to the Norg syntax, it is very inconvenient to have to manually
+update all of your files, especially if you're not a regex wizard.
+
+To alleviate this problem, the upgrade tool serves as a way to automate the process.
+
+There are two main commands that are exposed for use:
+- `:Neorg upgrade current-file` - takes the current file and upgrades it to the new syntax.
+  Will ask for a backup.
+- `:Neorg upgrade current-directory` - upgrades all files in the current directory. Asks
+   for a backup and displays the current directory so no mistakes are made.
+
+When a backup is requested, Neorg backs up the file to `<filename>.old.norg`, then upgrades
+the original file/directory in-place.
 --]]
 
 local module = neorg.modules.create("core.upgrade")
@@ -17,7 +31,10 @@ end
 
 module.config.public = {
     -- Whether to prompt the user to back up their file
-    -- every time they want to upgrade a `.norg` document.
+    -- every time they would like to upgrade a `.norg` document.
+    --
+    -- Unless you are certain of what you are doing, it's best to leave
+    -- this set to `true`.
     ask_for_backup = true,
 }
 
