156951bb6cd3eba2de43a3182e6e270806750f16
dark-ether <henrique.hsa2002@gmail.com>
2023-02-13 19:57:25 -0300

    ref(core.norg.dirman.summary): added code to generate the links

--- lua/neorg/modules/core/norg/dirman/summary/module.lua
+++ lua/neorg/modules/core/norg/dirman/summary/module.lua
@@ -5,6 +5,7 @@
 require("neorg.modules.base")
 require("neorg.modules")
 
+local log = require("neorg.external.log")
 local module = neorg.modules.create("core.norg.dirman.summary")
 
 module.setup = function()
@@ -32,7 +33,7 @@ module.config.public = {
             file = function()
                 return module.required["core.norg.dirman"].get_index()
             end,
-            -- The summary location, must be a heading.
+            -- The summary location, must be a top level heading.
             location = "* Index",
             -- File categories to include in the summary, if empty will include all notes
             categories = {},
@@ -44,8 +45,43 @@ module.public = {}
 
 module.events.subscribed = {
     ["core.neorgcmd"] = {
-        ["generate-workspace-summary"] = true,
+        ["dirman.summary"] = true,
     },
 }
-
+module.on_event = function(event)
+    local dirman = module.required["core.norg.dirman"]
+    if event.type == "core.neorgcmd.events.dirman.summary" then
+        local dir = dirman.get_current_workspace()[1]
+        local files = dirman.get_norg_files(dir)
+        local output = vim.defaulttable()
+        local function gen_string(tbl, name, all_parents, heading_level)
+            local str = heading_level .. name .. "\n"
+            if type(tbl) ~= "table" then
+                return name
+            end
+            for i, v in ipairs(tbl) do
+                tbl[i] = nil
+                str = str .. "{:" .. all_parents .. tostring(v) .. ":}[" .. tostring(v) .. "]\n"
+            end
+            for subdir, path in pairs(tbl) do
+                str = str .. gen_string(path, subdir, all_parents .. subdir .. "/", heading_level .. "*")
+            end
+            return str
+        end
+        for _, v in ipairs(files) do
+            v = string.reverse(string.gsub(string.reverse(v), "gron.", "", 1))
+            local path_list = vim.split(v, "/")
+            local tbl = output
+            for i, p in ipairs(path_list) do
+                path_list[i] = nil
+                if vim.tbl_isempty(path_list) then
+                    table.insert(tbl, p)
+                else
+                    tbl = tbl[p]
+                end
+            end
+        end
+        local str = gen_string(output, "", "", "*")
+    end
+end
 return module
