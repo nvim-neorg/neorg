73ca7b63c79a76d5cd8a3f0b39c5d171c1406fdc
Vhyrro <vhyrro@gmail.com>
2023-04-11 19:00:00 +0200

    feat(latex.renderer): implement rendering logic

--- lua/neorg/modules/core/latex/renderer/module.lua
+++ lua/neorg/modules/core/latex/renderer/module.lua
@@ -10,35 +10,108 @@ module.load = function()
     vim.api.nvim_create_autocmd("BufWinEnter", {
         pattern = "*.norg",
         callback = function(event)
-            module.required["core.integrations.treesitter"].execute_query([[
+            module.required["core.integrations.treesitter"].execute_query(
+                [[
                 (
                     (inline_math) @latex
                     (#offset! @latex 0 1 0 -1)
                 )
 
                 ; NOTE: We don't look for `@code` blocks here because those should be just that - code blocks, not rendered latex markup.
-                (ranged_verbatim_tag (tag_name) @_tagname (tag_parameters .(tag_param) @_language) (ranged_verbatim_tag_content) @latex (#eq? @_tagname "embed") (#eq? @_language "latex")
+                (ranged_verbatim_tag (tag_name) @_tagname (tag_parameters .(tag_param) @_language) (ranged_verbatim_tag_content) @latex (#eq? @_tagname "embed") (#eq? @_language "latex"))
                 (ranged_verbatim_tag (tag_name) @_tagname (tag_parameters)? (ranged_verbatim_tag_content) @latex (#eq? @_tagname "math"))
-            ]], function(query, id, node)
-                if query.captures[id] ~= "latex" then
-                    return
-                end
+            ]],
+                function(query, id, node)
+                    if query.captures[id] ~= "latex" then
+                        return
+                    end
 
-                local latex_snippet = module.required["core.integrations.treesitter"].get_node_text(node, event.buf)
+                    local latex_snippet = module.required["core.integrations.treesitter"].get_node_text(node, event.buf)
 
-                local parsed = module.public.parse_latex(latex_snippet)
+                    local png_location = module.public.parse_latex(latex_snippet)
 
-            end, event.buf)
+                    local renderer = neorg.modules.get_module(module.config.public.renderer)
+
+                    if not renderer then
+                        return
+                    end
+
+                    renderer.render(
+                        event.buf,
+                        png_location,
+                        module.required["core.integrations.treesitter"].get_node_range(node)
+                    )
+                end,
+                event.buf
+            )
         end,
     })
 end
 
 module.public = {
+    create_latex_document = function(snippet)
+        local tempname = vim.fn.tempname()
+
+        local tempfile = io.open(tempname, "w")
+
+        if not tempfile then
+            return
+        end
+
+        local content = table.concat(
+            {
+                "\\documentclass[12pt]{standalone}",
+                "\\usepackage{amsmath}",
+                "\\usepackage{amssymb}",
+                "\\begin{document}",
+                "\\begin{align}",
+                snippet,
+                "\\end{align}",
+                "\\end{document}",
+            },
+            "\n"
+        )
+
+        tempfile:write(content)
+        tempfile:close()
+
+        return tempname
+    end,
+
     -- Returns a handle to an image containing
     -- the rendered snippet.
     -- This handle can then be delegated to an external renderer.
     parse_latex = function(snippet)
+        local document_name = module.public.create_latex_document(snippet)
 
+        if not document_name then
+            return
+        end
+
+        local cwd = vim.fn.fnamemodify(document_name, ":h")
+        vim.fn.jobwait({
+            vim.fn.jobstart(
+                "latex  --interaction=nonstopmode --output-dir=" .. cwd .. " --output-format=dvi " .. document_name,
+                { cwd = cwd }
+            ),
+        })
+
+        local png_result = vim.fn.tempname()
+        -- TODO: Make the conversions async via `on_exit`
+        vim.fn.jobwait({
+            vim.fn.jobstart(
+                "dvipng -D "
+                    .. tostring(module.config.public.dpi)
+                    .. " -T tight -bg Transparent -fg 'cmyk 0.00 0.04 0.21 0.02' -o "
+                    .. png_result
+                    .. " "
+                    .. document_name
+                    .. ".dvi",
+                { cwd = vim.fn.fnamemodify(document_name, ":h") }
+            ),
+        })
+
+        return png_result
     end,
 }
 
