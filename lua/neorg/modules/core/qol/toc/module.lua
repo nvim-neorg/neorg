cc1adca40dba30b84820f35935f2d2570768b486
Vhyrro <vhyrro@gmail.com>
2023-04-27 20:23:47 +0200

    docs(core.qol.toc): fix buggy documentation rendering

--- lua/neorg/modules/core/qol/toc/module.lua
+++ lua/neorg/modules/core/qol/toc/module.lua
@@ -4,7 +4,9 @@
     description: The TOC module geneates a table of contents for a given Norg buffer.
     summary: Generates a table of contents for a given Norg buffer.
     ---
+
 <!-- TODO: make nested objects also appear nested within the TOC view (i.e. headings in headings) --!>
+
 The TOC module exposes a single command - `:Neorg toc`. This command can be executed with one of three
 optional arguments: `left`, `right` and `qflist`.
 
