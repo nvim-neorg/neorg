5e804e1a959f6b70471070acfd87fe9e86c01149
Vhyrro <vhyrro@gmail.com>
2023-05-01 16:37:09 +0200

    refactor: allow `norg` highlights in `@code` blocks again

--- queries/norg/injections.scm
+++ queries/norg/injections.scm
@@ -1,5 +1,5 @@
 ; Injection for code blocks
-(ranged_verbatim_tag (tag_name) @_tagname (tag_parameters .(tag_param) @language) (ranged_verbatim_tag_content) @content (#any-of? @_tagname "code" "embed") (#not-eq? @language "norg"))
+(ranged_verbatim_tag (tag_name) @_tagname (tag_parameters .(tag_param) @language) (ranged_verbatim_tag_content) @content (#any-of? @_tagname "code" "embed"))
 (ranged_verbatim_tag (tag_name) @_tagname (tag_parameters)? (ranged_verbatim_tag_content) @latex (#eq? @_tagname "math"))
 
 (
