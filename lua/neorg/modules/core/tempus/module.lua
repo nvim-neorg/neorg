c2a293776acc7a6b02f30a34141f3134600a44e3
vhyrro <vhyrro@users.noreply.github.com>
2023-09-08 16:46:45 +0000

    chore: autoformat with stylua

--- lua/neorg/modules/core/tempus/module.lua
+++ lua/neorg/modules/core/tempus/module.lua
@@ -231,14 +231,17 @@ module.public = {
     ---@param parsed_date Date #The date to convert
     ---@return osdate #A Lua date
     to_lua_date = function(parsed_date)
-        return os.date("*t", os.time(vim.tbl_deep_extend("force", os.date("*t"), {
-            day = parsed_date.day,
-            month = parsed_date.month and parsed_date.month.number or nil,
-            year = parsed_date.year,
-            hour = parsed_date.time and parsed_date.time.hour,
-            min = parsed_date.time and parsed_date.time.minute,
-            sec = parsed_date.time and parsed_date.time.second,
-        })))
+        return os.date(
+            "*t",
+            os.time(vim.tbl_deep_extend("force", os.date("*t"), {
+                day = parsed_date.day,
+                month = parsed_date.month and parsed_date.month.number or nil,
+                year = parsed_date.year,
+                hour = parsed_date.time and parsed_date.time.hour,
+                min = parsed_date.time and parsed_date.time.minute,
+                sec = parsed_date.time and parsed_date.time.second,
+            }))
+        )
     end,
 
     --- Converts a lua `osdate` to a Neorg date.
