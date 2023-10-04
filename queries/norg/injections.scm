2693f1546cd2e931cab47d2ba8da98d6f05d035a
Vhyrro <vhyrro@gmail.com>
2023-10-04 17:10:10 +0200

    fix: temporarily remove `inline_math` highlights

--- queries/norg/injections.scm
+++ queries/norg/injections.scm
@@ -2,10 +2,10 @@
 (verbatim_ranged_tag (tag_name) @_tagname .(parameter) @injection.language (verbatim_content) @injection.content (#any-of? @_tagname "code" "embed"))
 (verbatim_ranged_tag (tag_name) @_tagname (verbatim_content) @injection.content (#eq? @_tagname "math") (#set! injection.language "latex"))
 
-(
-    (inline_math) @injection.content
-    (#offset! @injection.content 0 1 0 -1)
-    (#set! injection.language "latex")
-)
+; (
+;     (inline_math) @injection.content
+;     (#offset! @injection.content 0 1 0 -1)
+;     (#set! injection.language "latex")
+; )
 
 (verbatim_ranged_tag (tag_name) @_tagname (verbatim_content) @injection.content (#eq? @_tagname "document.meta") (#set! injection.language "norg_meta"))
