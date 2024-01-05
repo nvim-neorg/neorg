14de1663af6394d73f4d9705711c126ba7122840
Vhyrro <vhyrro@gmail.com>
2024-01-05 22:11:07 +0100

    docs(neorg/core/utils): add LuaCATS documentation

--- lua/neorg/core/utils.lua
+++ lua/neorg/core/utils.lua
@@ -7,16 +7,18 @@ local version = vim.version() -- TODO: Move to a more local scope
 --- A version agnostic way to call the neovim treesitter query parser
 --- @param language string # Language to use for the query
 --- @param query_string string # Query in s-expr syntax
---- @return any # Parsed query
+--- @return Query # Parsed query
 function utils.ts_parse_query(language, query_string)
     if vim.treesitter.query.parse then
         return vim.treesitter.query.parse(language, query_string)
     else
-        return vim.treesitter.parse_query(language, query_string) ---@diagnostic disable-line -- TODO: type error workaround <pysan3>
+        ---@diagnostic disable-next-line
+        return vim.treesitter.parse_query(language, query_string)
     end
 end
 
 --- An OS agnostic way of querying the current user
+--- @return string username
 function utils.get_username()
     local current_os = configuration.os_info
 
@@ -33,26 +35,31 @@ function utils.get_username()
     return ""
 end
 
---- Returns an array of strings, the array being a list of languages that Neorg can inject
----@param values boolean #If set to true will return an array of strings, if false will return a key-value table
+--- Returns an array of strings, the array being a list of languages that Neorg can inject.
+---@param values boolean If set to true will return an array of strings, if false will return a key-value table.
+---@return string[]|table<string, { type: "treesitter"|"syntax" }>
 function utils.get_language_list(values)
     local regex_files = {}
     local ts_files = {}
-    -- search for regex files in syntax and after/syntax
-    -- its best if we strip out anything but the ft name
+
+    -- Search for regex files in syntax and after/syntax.
+    -- Its best if we strip out anything but the ft name.
     for _, lang in pairs(vim.api.nvim_get_runtime_file("syntax/*.vim", true)) do
         local lang_name = vim.fn.fnamemodify(lang, ":t:r")
         table.insert(regex_files, lang_name)
     end
+
     for _, lang in pairs(vim.api.nvim_get_runtime_file("after/syntax/*.vim", true)) do
         local lang_name = vim.fn.fnamemodify(lang, ":t:r")
         table.insert(regex_files, lang_name)
     end
-    -- search for available parsers
+
+    -- Search for available parsers
     for _, parser in pairs(vim.api.nvim_get_runtime_file("parser/*.so", true)) do
-        local parser_name = vim.fn.fnamemodify(parser, ":t:r")
+        local parser_name = assert(vim.fn.fnamemodify(parser, ":t:r"))
         ts_files[parser_name] = true
     end
+
     local ret = {}
 
     for _, syntax in pairs(regex_files) do
@@ -66,7 +73,11 @@ function utils.get_language_list(values)
     return values and vim.tbl_keys(ret) or ret
 end
 
+--- Gets a list of shorthands for a given language.
+--- @param reverse_lookup boolean Whether to create a reverse lookup for the table.
+--- @return LanguageList
 function utils.get_language_shorthands(reverse_lookup)
+    ---@class LanguageList
     local langs = {
         ["bash"] = { "sh", "zsh" },
         ["c_sharp"] = { "csharp", "cs" },
@@ -98,11 +109,11 @@ function utils.get_language_shorthands(reverse_lookup)
     return reverse_lookup and vim.tbl_add_reverse_lookup(langs) or langs
 end
 
---- Checks whether Neovim is running at least at a specific version
----@param major number #The major release of Neovim
----@param minor number #The minor release of Neovim
----@param patch number #The patch number (in case you need it)
----@return boolean #Whether Neovim is running at the same or a higher version than the one given
+--- Checks whether Neovim is running at least at a specific version.
+--- @param major number The major release of Neovim.
+--- @param minor number The minor release of Neovim.
+--- @param patch number The patch number (in case you need it).
+--- @return boolean # Whether Neovim is running at the same or a higher version than the one given.
 function utils.is_minimum_version(major, minor, patch)
     if major ~= version.major then
         return major < version.major
@@ -117,16 +128,18 @@ function utils.is_minimum_version(major, minor, patch)
 end
 
 --- Parses a version string like "0.4.2" and provides back a table like { major = <number>, minor = <number>, patch = <number> }
----@param version_string string #The input string
----@return table #The parsed version string, or `nil` if a failure occurred during parsing
+--- @param version_string string The input string.
+--- @return table? # The parsed version string, or `nil` if a failure occurred during parsing.
 function utils.parse_version_string(version_string)
     if not version_string then
-        return ---@diagnostic disable-line -- TODO: type error workaround <pysan3>
+        return
     end
 
     -- Define variables that split the version up into 3 slices
     local split_version, versions, ret =
-        vim.split(version_string, ".", true), { "major", "minor", "patch" }, { major = 0, minor = 0, patch = 0 } ---@diagnostic disable-line -- TODO: type error workaround <pysan3>
+        vim.split(version_string, ".", { plain = true }),
+        { "major", "minor", "patch" },
+        { major = 0, minor = 0, patch = 0 }
 
     -- If the sliced version string has more than 3 elements error out
     if #split_version > 3 then
@@ -135,7 +148,7 @@ function utils.parse_version_string(version_string)
             version_string,
             "failed - too many version numbers provided. Version should follow this layout: <major>.<minor>.<patch>"
         )
-        return ---@diagnostic disable-line -- TODO: type error workaround <pysan3>
+        return
     end
 
     -- Loop through all the versions and check whether they are valid numbers. If they are, add them to the return table
@@ -145,7 +158,7 @@ function utils.parse_version_string(version_string)
 
             if not num then
                 log.warn("Invalid version provided, string cannot be converted to integral type.")
-                return ---@diagnostic disable-line -- TODO: type error workaround <pysan3>
+                return
             end
 
             ret[ver] = num
@@ -155,16 +168,16 @@ function utils.parse_version_string(version_string)
     return ret
 end
 
---- Custom neorg notifications. Wrapper around vim.notify
----@param msg string message to send
----@param log_level integer|nil log level in `vim.log.levels`.
+--- Custom Neorg notifications. Wrapper around `vim.notify`.
+--- @param msg string Message to send.
+--- @param log_level integer? Log level in `vim.log.levels`.
 function utils.notify(msg, log_level)
     vim.notify(msg, log_level, { title = "Neorg" })
 end
 
 --- Opens up an array of files and runs a callback for each opened file.
----@param files string[] #An array of files to open.
----@param callback fun(buffer: integer, filename: string) #The callback to invoke for each file.
+--- @param files string[] An array of files to open.
+--- @param callback fun(buffer: integer, filename: string) The callback to invoke for each file.
 function utils.read_files(files, callback)
     for _, file in ipairs(files) do
         local bufnr = vim.uri_to_bufnr(vim.uri_from_fname(file))
@@ -195,7 +208,8 @@ function utils.wrap_dotrepeat(event_handler)
         utils._neorg_is_dotrepeat = false
         utils.set_operatorfunc(function()
             if utils._neorg_is_dotrepeat then
-                local pos = vim.fn.getpos(".")
+                local pos = assert(vim.fn.getpos("."))
+
                 event.buffer = pos[1]
                 event.cursor_position = { pos[2], pos[3] }
             end
@@ -206,10 +220,10 @@ function utils.wrap_dotrepeat(event_handler)
     end
 end
 
---- Truncate input str to fit inside the col_limit when displayed. Takes non-ascii chars into account.
----@param str string
----@param col_limit integer #str will be cut so that when displayed, the display length does not exceed limit
----@return string #substring of input str
+--- Truncate input string to fit inside the `col_limit` when displayed. Takes non-ascii chars into account.
+--- @param str string The string to limit.
+--- @param col_limit integer `str` will be cut so that when displayed, the display length does not exceed this limit.
+--- @return string # Substring of input str
 function utils.truncate_by_cell(str, col_limit)
     if str and str:len() == vim.api.nvim_strwidth(str) then
         return vim.fn.strcharpart(str, 0, col_limit)
