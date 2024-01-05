cf961dcd15ea628c47dfe4ec479e1a82038c57fe
Vhyrro <vhyrro@gmail.com>
2024-01-05 19:42:20 +0100

    docs(neorg/core/config): add LuaCATS annotations

--- lua/neorg/core/config.lua
+++ lua/neorg/core/config.lua
@@ -1,4 +1,34 @@
--- Grab OS info on startup
+--- @brief [[
+--- Defines the configuration table for use throughout Neorg.
+--- @brief ]]
+
+-- TODO(vhyrro): Make `norg_version` and `version` a `Version` class.
+
+--- @alias OperatingSystem
+--- | "windows"
+--- | "wsl"
+--- | "wsl2"
+--- | "mac"
+--- | "linux"
+
+--- @alias neorg.configuration.module { config?: table }
+
+--- @class (exact) neorg.configuration.user
+--- @field lazy_loading boolean                              Whether to defer loading the Neorg core until after the user has entered a `.norg` file.
+--- @field load table<string, neorg.configuration.module>    A list of modules to load, alongside their configurations.
+
+--- @class (exact) neorg.configuration
+--- @field user_config neorg.configuration.user              Stores the configuration provided by the user.
+--- @field modules table<string, neorg.configuration.module> Acts as a copy of the user's configuration that may be modified at runtime.
+--- @field manual boolean?                                   Used if Neorg was manually loaded via `:NeorgStart`. Only applicable when `user_config.lazy_loading` is `true`.
+--- @field arguments table<string, string>                   A list of arguments provided to the `:NeorgStart` function in the form of `key=value` pairs. Only applicable when `user_config.lazy_loading` is `true`.
+--- @field norg_version string                               The version of the file format to be used throughout Neorg. Used internally.
+--- @field version string                                    The version of Neorg that is currently active. Automatically updated by CI on every release.
+--- @field os_info OperatingSystem                           The operating system that Neorg is currently running under.
+--- @field pathsep "\\"|"/"                                  The operating system that Neorg is currently running under.
+
+--- Gets the current operating system.
+--- @return OperatingSystem
 local function get_os_info()
     local os = vim.loop.os_uname().sysname:lower()
 
@@ -19,11 +49,18 @@ local function get_os_info()
         end
         return "linux"
     end
+
+    error("[neorg]: Unable to determine the currently active operating system!")
 end
 
 local os_info = get_os_info()
 
--- Configuration template
+--- Stores the configuration for the entirety of Neorg.
+--- This includes not only the user configuration (passed to `setup()`), but also internal
+--- variables that describe something specific about the user's hardware.
+--- @see neorg.setup
+---
+--- @type neorg.configuration
 local config = {
     user_config = {
         lazy_loading = false,
