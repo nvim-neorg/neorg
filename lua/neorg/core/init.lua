5fcae0b080531ac1438faeefd47ae11e1633b463
Vhyrro <vhyrro@gmail.com>
2024-03-01 16:13:59 +0100

    fix: refactor library to not use lua utils

--- lua/neorg/core/init.lua
+++ lua/neorg/core/init.lua
@@ -1,6 +1,7 @@
 local neorg = {
     callbacks = require("neorg.core.callbacks"),
     config = require("neorg.core.config"),
+    log = require("neorg.core.log"),
     modules = require("neorg.core.modules"),
     utils = require("neorg.core.utils"),
 }
