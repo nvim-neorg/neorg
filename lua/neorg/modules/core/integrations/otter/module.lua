46733a5d435dca05b05fe97f89e316acea3f74bb
benlubas <benmlubas@gmail.com>
2024-03-02 01:16:29 -0500

    feat: integrations.otter

--- lua/neorg/modules/core/integrations/otter/module.lua
+++ lua/neorg/modules/core/integrations/otter/module.lua
@@ -15,11 +15,11 @@ Otter.nvim is a plugin that gives you LSP features in embedded languages. The LS
 - range formatting (not document formatting)
 
 ## Setup
-You need to install otter.nvim, and you can configure it yourself based on the Otter.nvim README.
+You need to install otter.nvim and make sure it's loaded before Neorg, you can configure it yourself
+by reading [the Otter.nvim README](https://github.com/jmbuhr/otter.nvim).
 
-Add this to your Neorg configuration:
-```lua
-["integrations.otter"] = {}, -- add your configuration here
+If you want auto complete, make sure you add `"otter"` as a source to nvim-cmp (detailed in the
+otter README)
 ```
 
 ## Commands
@@ -45,39 +45,51 @@ module.load = function()
     ok, otter = pcall(require, "otter")
     assert(ok, "[Neorg] Failed to load otter.nvim")
 
-    if module.public.config.auto_start then
-        print("add some type of ftplugin or auto command")
+    if module.config.public.auto_start then
+        local group = vim.api.nvim_create_augroup("neorg.integrations.otter", {})
+        vim.api.nvim_create_autocmd({ "BufEnter" }, {
+            desc = "Activate Otter on Buf Enter",
+            pattern = "*.norg",
+            group = group,
+            callback = function(_)
+                module.public.activate()
+            end
+        })
     end
 
-        module.required["core.neorgcmd"].add_commands_from_table({
-        refactor = {
-            min_args = 0,
-            max_args = 1,
-            name = "refactor",
+    module.required["core.neorgcmd"].add_commands_from_table({
+        otter = {
+            args = 1,
+            name = "otter",
             condition = "norg",
             subcommands = {
-                rename = {
-                    args = 1,
-                    name = "refactor.rename",
-                    subcommands = {
-                        file = {
-                            min_args = 0,
-                            max_args = 1,
-                            name = "refactor.rename.file",
-                        },
-                        heading = {
-                            args = 0,
-                            name = "refactor.rename.heading",
-                        },
-                    },
+                enable = {
+                    args = 0,
+                    name = "otter.enable",
+                },
+                disable = {
+                    args = 0,
+                    name = "otter.disable",
                 },
             },
         },
     })
-
 end
 
-module.public.config = {
+module.events.subscribed = {
+    ["core.neorgcmd"] = {
+        ["otter.enable"] = true,
+        ["otter.disable"] = true,
+    },
+}
+
+module.on_event = function(event)
+    if module.private[event.split_type[2]] then
+        module.private[event.split_type[2]](event)
+    end
+end
+
+module.config.public = {
     -- list of languages that otter will try to start a language server for. nil means all languages
     languages = nil,
 
@@ -107,21 +119,26 @@ module.public.config = {
 }
 
 module.private = {
-    status = false,
+    ["otter.enable"] = function(_)
+        module.public.activate()
+    end,
+    ["otter.disable"] = function(_)
+        module.public.deactivate()
+    end,
 }
 
 module.public = {
     ---Activate otter in the current buffer, includes setting buffer keymaps
     activate = function()
         otter.activate(
-            module.public.config.languages,
-            module.public.config.completion.enabled,
-            module.public.config.diagnostics.enabled,
+            module.config.public.languages,
+            module.config.public.completion.enabled,
+            module.config.public.diagnostics.enabled,
             nil -- or a query...
         )
 
         local b = vim.api.nvim_get_current_buf()
-        for func, lhs in pairs(module.public.config.keys) do
+        for func, lhs in pairs(module.config.public.keys) do
             vim.api.nvim_buf_set_keymap(
                 b,
                 "n",
@@ -136,8 +153,12 @@ module.public = {
     deactivate = function()
         -- TODO: there is no current way to deactivate otter. I have an open issue for this here
         -- https://github.com/jmbuhr/otter.nvim/issues/94
+        otter.deactivate(
+            module.config.public.completion.enabled,
+            module.config.public.diagnostics.enabled
+        )
         local b = vim.api.nvim_get_current_buf()
-        for _, lhs in pairs(module.public.config.keys) do
+        for _, lhs in pairs(module.config.public.keys) do
             vim.api.nvim_buf_del_keymap(b, "n", lhs)
         end
     end,
