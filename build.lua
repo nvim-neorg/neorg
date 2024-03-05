6ea1eff15d3f1fa947255a94f99cadb298c8b66f
Vhyrro <vhyrro@gmail.com>
2024-03-05 19:28:05 +0100

    feat: include plenary as a dependency

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 -- This build.lua exists to bridge luarocks installation for lazy.nvim users.
 -- It's main purposes are:
 -- - Shelling out to luarocks.nvim for installation
--- - Installing neorg as a rock (including dependencies)
+-- - Installing neorg's dependencies as rocks
 
 -- Important note: we execute the build code in a vim.schedule
 -- to defer the execution and ensure that the runtimepath is appropriately set.
@@ -9,13 +9,12 @@
 vim.schedule(function()
     local ok, luarocks = pcall(require, "luarocks.rocks")
 
-    assert(ok, "Unable to install neorg: required dependency `camspiers/luarocks` not found!")
-
-    -- local version = require("neorg.core.config").version
+    assert(ok, "Unable to install neorg: required dependency `vhyrro/luarocks.nvim` not found!")
 
     luarocks.ensure({
         "nvim-nio ~> 1.7",
         "lua-utils.nvim == 1.0.2",
+        "plenary.nvim == 0.1.4",
     })
 
     require("neorg").setup_after_build()
