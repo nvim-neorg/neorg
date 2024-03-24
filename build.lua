ceaf82c2977b61eb02ba45edf02d09f9d155c7ac
Vhyrro <vhyrro@gmail.com>
2024-03-24 16:52:41 +0100

    revert 'fix: perform setup after the parsers have been installed'

--- build.lua
+++ build.lua
@@ -20,6 +20,6 @@ vim.schedule(function()
 
     package.loaded["neorg"] = nil
 
-    pcall(vim.cmd.Neorg, "sync-parsers")
     require("neorg").setup_after_build()
+    pcall(vim.cmd.Neorg, "sync-parsers")
 end)
