b1f408a52fd76503eea49d84aefdd73bde415506
b4mbus <danielzaradny@gmail.com>
2024-05-26 23:33:35 +0200

    ref(nix): inline shell module

--- nix/shells/default.nix
+++ nix/shells/default.nix
@@ -1,3 +1,17 @@
 {pkgs, ...}: {
-  devShells.default = pkgs.callPackage ./neorg-shell.nix {};
+  devShells.default = pkgs.mkShell {
+    name = "neorg devShell";
+
+    shellHook = ''
+      ln -fs ${pkgs.luarc-to-json pkgs.luarc-with-dependencies} .luarc.json
+    '';
+
+    packages = with pkgs; [
+      lua-language-server
+      stylua
+      lua51Packages.luacheck
+      nil
+      lua5_1
+    ];
+  };
 }
