481523f2e8e0b2e608cce9c3145a5fe66afa2667
squont <scope@yden.ro>
2024-06-24 11:18:25 +1000

    update to reflect correct true-zen folder structure

--- lua/neorg/modules/core/integrations/truezen/module.lua
+++ lua/neorg/modules/core/integrations/truezen/module.lua
@@ -4,18 +4,23 @@
     summary: Integrates the TrueZen module for use within Neorg.
     internal: true
     ---
-This is a basic wrapper around truezen that allows one to toggle the atraxis mode programatically.
+This is a basic wrapper around truezen that allows one to toggle the atraxis mode programmatically.
 --]]
 
 local neorg = require("neorg.core")
-local modules = neorg.modules
+local modules, log = neorg.modules, neorg.log
 
 local module = modules.create("core.integrations.truezen")
 
+
 module.setup = function()
     local success, truezen = pcall(require, "true-zen.main")
+    local success, truezen = pcall(require, "true-zen")
+
 
     if not success then
+        log.warn("Could not find module: `true-zen`. Please ensure you have true-zen installed.")
+
         return { success = false }
     end
 
@@ -34,3 +39,4 @@ module.public = {
 }
 
 return module
+
