ed79914a413100e37b36189f6cbb3006236d01ba
Patrick Armstrong <patrick.james.1998@gmail.com>
2025-01-15 14:11:57 +1100

    Fix #1624

--- lua/neorg/modules/core/summary/module.lua
+++ lua/neorg/modules/core/summary/module.lua
@@ -367,7 +367,7 @@ module.on_event = function(event)
 	-- So that :Neorg generate_workspace_summary a b c
 	-- Will result in include_categories = {"a", "b", "c"}
 	local include_categories = {table.unpack(event.content.data.fargs, 2)}
-        module.public.generate_workspace_summary(event.buffer, event.cursor_position, event.content)
+        module.public.generate_workspace_summary(event.buffer, event.cursor_position, include_categories)
     end
 end
 
