a84ec000cca734e7d771b6aca5b688aad8d8ec96
Patrick Armstrong <patrick.james.1998@gmail.com>
2025-01-15 14:05:24 +1100

    Fix #1624

--- lua/neorg/modules/core/summary/module.lua
+++ lua/neorg/modules/core/summary/module.lua
@@ -363,6 +363,10 @@ module.events.subscribed = {
 
 module.on_event = function(event)
     if event.type == "core.neorgcmd.events.summary.summarize" then
+	-- Extract any additional arguments past the first
+	-- So that :Neorg generate_workspace_summary a b c
+	-- Will result in include_categories = {"a", "b", "c"}
+	local include_categories = {table.unpack(event.content.data.fargs, 2)}
         module.public.generate_workspace_summary(event.buffer, event.cursor_position, event.content)
     end
 end
