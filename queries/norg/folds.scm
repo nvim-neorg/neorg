431de0a233ce5ab86da4b4e03786d6fe568daf62
Ben Lubas <56943754+benlubas@users.noreply.github.com>
2025-06-10 15:00:13 -0400

    feat: foldable lists and ranged verbatim (#1690)

--- queries/norg/folds.scm
+++ queries/norg/folds.scm
@@ -1,7 +1,4 @@
-(ranged_verbatim_tag
-    name: (tag_name) @_name
-    (#eq? @_name "document.meta")
-) @fold
+(ranged_verbatim_tag) @fold
 
 [
     (heading1)
@@ -10,4 +7,22 @@
     (heading4)
     (heading5)
     (heading6)
+] @fold
+
+[
+    (unordered_list1)
+    (unordered_list2)
+    (unordered_list3)
+    (unordered_list4)
+    (unordered_list5)
+    (unordered_list6)
+] @fold
+
+[
+    (ordered_list1)
+    (ordered_list2)
+    (ordered_list3)
+    (ordered_list4)
+    (ordered_list5)
+    (ordered_list6)
 ] @fold
