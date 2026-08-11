311877516b4641074e2bee9bfcd3b2ed284f466e
Randy West <randywest55@gmail.com>
2026-08-11 15:14:41 -0700

    Reselect visual area after vmode pro/demote range
    Addresses #1827

--- lua/neorg/modules/core/keybinds/module.lua
+++ lua/neorg/modules/core/keybinds/module.lua
@@ -392,9 +392,9 @@ module.private = {
 
                 v = {
                     -- Promote objects in range
-                    { ">", "<Plug>(neorg.promo.promote.range)", opts = { desc = "[neorg] Promote Objects in Range" } },
+                    { ">", "<Plug>(neorg.promo.promote.range)gv", opts = { desc = "[neorg] Promote Objects in Range" } },
                     -- Demote objects in range
-                    { "<", "<Plug>(neorg.promo.demote.range)", opts = { desc = "[neorg] Demote Objects in Range" } },
+                    { "<", "<Plug>(neorg.promo.demote.range)gv", opts = { desc = "[neorg] Demote Objects in Range" } },
                 },
             },
         },
